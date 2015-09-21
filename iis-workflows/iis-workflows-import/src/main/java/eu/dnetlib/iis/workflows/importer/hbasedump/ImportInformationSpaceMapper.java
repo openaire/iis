dcae28630c782ebc75f501c435bbee355bd287d4
Marek Horst <mhorst@icm.edu.pl>
2015-09-21 11:14:33 +0200

    utilizing TableOutputFormat instead of explicit writing to hbase to simplify importer module

--- iis-workflows/iis-workflows-import/src/main/java/eu/dnetlib/iis/workflows/importer/hbasedump/ImportInformationSpaceMapper.java
+++ iis-workflows/iis-workflows-import/src/main/java/eu/dnetlib/iis/workflows/importer/hbasedump/ImportInformationSpaceMapper.java
@@ -1,16 +1,11 @@
 package eu.dnetlib.iis.workflows.importer.hbasedump;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -24,69 +19,38 @@ import eu.dnetlib.data.proto.OafProtos.Oaf;
  *
  */
 public class ImportInformationSpaceMapper 
-	extends Mapper<Text, Text, NullWritable, NullWritable> {
+	extends Mapper<Text, Text, ImmutableBytesWritable, Put> {
 
-	private static final String OUTPUT_TABLE_NAME = "output.table.name";
-	
 	private static final char KEY_SEPARATOR = '@';
+	
+	private ImmutableBytesWritable ibw;
 
-	private HTableInterface hTable;
-	
-	private List<Put> putCache = new ArrayList<Put>();
-	
-	private int cacheThreshold = 1000;
-	
 	@Override
-	protected void setup(
-			Mapper<Text, Text, NullWritable, NullWritable>.Context context)
-			throws IOException, InterruptedException {
-		String tableName = context.getConfiguration().get(
-				OUTPUT_TABLE_NAME);
-		if (tableName==null) {
-			throw new IOException("hbase table name was not provided!");
-		}
-		hTable = new HTable(HBaseConfiguration.create(
-				context.getConfiguration()), tableName);
+	protected void setup(final Context context) throws IOException, InterruptedException {
+		super.setup(context);
+		ibw = new ImmutableBytesWritable();
 	}
 	
 	@Override
 	protected void map(Text key, Text value,
-			Mapper<Text, Text, NullWritable, NullWritable>.Context context)
+			Mapper<Text, Text, ImmutableBytesWritable, Put>.Context context)
 			throws IOException, InterruptedException {
 //		rowkey@columnFamily@qualifier
 		String oafJson = value.toString();
-		if (!oafJson.isEmpty()) {
+		if (StringUtils.isNotBlank(oafJson)) {
 			String[] split = StringUtils.split(key.toString(), KEY_SEPARATOR);
 			Oaf.Builder oafBuilder = Oaf.newBuilder();
 			JsonFormat.merge(oafJson, oafBuilder);
-			Put put = new Put(Bytes.toBytes(split[0]));
+			byte[] rowKey = Bytes.toBytes(split[0]);
+			Put put = new Put(rowKey);
 			put.add(Bytes.toBytes(split[1]), 
 					Bytes.toBytes(split[2]), 
 					oafBuilder.build().toByteArray());
-//			skipping writing to WAL
+//			skipping writing to WAL to speed up importing process
 			put.setWriteToWAL(false);
-			putCache.add(put);
-			if (putCache.size()>cacheThreshold) {
-				hTable.put(putCache);
-				putCache.clear();
-			}
+			ibw.set(rowKey);
+			context.write(ibw, put);
 		}	
-	}
-	
-	@Override
-	protected void cleanup(Context context)
-			throws IOException, InterruptedException {
-		if (hTable!=null) {
-			try {
-				if (!putCache.isEmpty()) {
-					hTable.put(putCache);
-					putCache.clear();
-				}	
-			} finally {
-				hTable.flushCommits();
-				hTable.close();
-			}
-		}
 	}
 	
 }
