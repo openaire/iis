5d0e7c077b1fe6857306bf97b97f015873b2cbab
Marek Horst <mhorst@icm.edu.pl>
2015-09-21 11:58:48 +0200

    redmine:1516 integrating SFI projects reference extraction

--- iis-workflows/iis-workflows-referenceextraction/src/test/java/eu/dnetlib/iis/workflows/referenceextraction/project/WorkflowTest.java
+++ iis-workflows/iis-workflows-referenceextraction/src/test/java/eu/dnetlib/iis/workflows/referenceextraction/project/WorkflowTest.java
@@ -1,9 +1,10 @@
 package eu.dnetlib.iis.workflows.referenceextraction.project;
 
-import eu.dnetlib.iis.IntegrationTest;
-import eu.dnetlib.iis.core.AbstractWorkflowTestCase;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+
+import eu.dnetlib.iis.IntegrationTest;
+import eu.dnetlib.iis.core.AbstractWorkflowTestCase;
 
 /**
  * 
@@ -16,11 +17,6 @@ public class WorkflowTest extends AbstractWorkflowTestCase {
     @Test
 	public void testMainWorkflow() throws Exception {
     	runWorkflow("eu/dnetlib/iis/workflows/referenceextraction/project/main/sampletest/oozie_app");
-	}
-
-    @Test
-	public void testMainSQLiteWorkflow() throws Exception{
-		runWorkflow("eu/dnetlib/iis/workflows/referenceextraction/project/main_sqlite/sampletest/oozie_app");
 	}
 
     @Test
