7d2a5b289acde2bab523b567f1c3e673642087be
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2015-09-23 14:29:43 +0200

    junit.framework -> org.junit in imports

--- iis-3rdparty-pig-avrostorage/src/test/java/org/apache/pig/piggybank/test/storage/avro/TestAvroStorage.java
+++ iis-3rdparty-pig-avrostorage/src/test/java/org/apache/pig/piggybank/test/storage/avro/TestAvroStorage.java
@@ -16,7 +16,7 @@
  */
 package org.apache.pig.piggybank.test.storage.avro;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
