4d9fb5f799048e1f68592a344d8e1e995dc62c32
Marek Horst <mhorst@icm.edu.pl>
2015-10-21 19:30:59 +0200

    satisfying pull reqest comments: fixing indention and shared method for handing exception in MetadataImporter

--- iis-workflows/iis-workflows-ingest-pmc/src/main/java/eu/dnetlib/iis/workflows/ingest/pmc/metadata/MetadataImporter.java
+++ iis-workflows/iis-workflows-ingest-pmc/src/main/java/eu/dnetlib/iis/workflows/ingest/pmc/metadata/MetadataImporter.java
@@ -24,7 +24,6 @@ import eu.dnetlib.iis.core.javamapreduce.MultipleOutputs;
 import eu.dnetlib.iis.ingest.pmc.metadata.schemas.ExtractedDocumentMetadata;
 import eu.dnetlib.iis.metadataextraction.schemas.DocumentText;
 
-
 /**
  * @author Michal Oniszczuk (m.oniszczuk@icm.edu.pl)
  * @author mhorst
@@ -32,14 +31,14 @@ import eu.dnetlib.iis.metadataextraction.schemas.DocumentText;
 public class MetadataImporter extends Mapper<AvroKey<DocumentText>, NullWritable, NullWritable, NullWritable> {
 
 	protected final Logger log = Logger.getLogger(this.getClass());
-	
+
 	public static final String FAULT_TEXT = "text";
-	
+
 	/**
 	 * Multiple outputs.
 	 */
 	protected MultipleOutputs mos = null;
-	
+
 	/**
 	 * Document metadata named output.
 	 */
@@ -49,65 +48,74 @@ public class MetadataImporter extends Mapper<AvroKey<DocumentText>, NullWritable
 	 * Fault named output.
 	 */
 	protected String namedOutputFault;
-	
-    @Override
+
+	@Override
 	protected void setup(Mapper<AvroKey<DocumentText>, NullWritable, NullWritable, NullWritable>.Context context)
 			throws IOException, InterruptedException {
-    	namedOutputMeta = context.getConfiguration().get("output.meta");
-		if (namedOutputMeta==null || namedOutputMeta.isEmpty()) {
+		namedOutputMeta = context.getConfiguration().get("output.meta");
+		if (namedOutputMeta == null || namedOutputMeta.isEmpty()) {
 			throw new RuntimeException("no named output provided for metadata");
 		}
 		namedOutputFault = context.getConfiguration().get("output.fault");
-		if (namedOutputFault==null || namedOutputFault.isEmpty()) {
+		if (namedOutputFault == null || namedOutputFault.isEmpty()) {
 			throw new RuntimeException("no named output provided for fault");
 		}
-    	mos = new MultipleOutputs(context);
+		mos = new MultipleOutputs(context);
 	}
 
 	@Override
-    protected void map(AvroKey<DocumentText> key, NullWritable value, Context context)
-            throws IOException, InterruptedException {
-        DocumentText nlm = key.datum();
-        if (!StringUtils.isBlank(nlm.getText())) {
-            final ExtractedDocumentMetadata.Builder output = ExtractedDocumentMetadata.newBuilder();
-            output.setId(nlm.getId());
-            try {
-//            	disabling validation
-    			SAXParserFactory saxFactory = SAXParserFactory.newInstance();
-    			saxFactory.setValidating(false);
-    			SAXParser saxParser = saxFactory.newSAXParser();
-    			XMLReader reader = saxParser.getXMLReader();
-    			reader.setFeature("http://xml.org/sax/features/validation", false);
-    			reader.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
-    			reader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-    			
-    			PmcXmlHandler pmcXmlHandler = new PmcXmlHandler(output);
-				saxParser.parse(new InputSource(
-						new StringReader(nlm.getText().toString())), 
-						pmcXmlHandler);
-				mos.write(namedOutputMeta, 
-						new AvroKey<ExtractedDocumentMetadata>(output.build()));
-        	 } catch (ParserConfigurationException e) {
-        		 Map<CharSequence, CharSequence> auditSupplementaryData = new HashMap<CharSequence, CharSequence>();
-        			auditSupplementaryData.put(FAULT_TEXT, nlm.getText());
-        		 mos.write(namedOutputFault, 
-        				 new AvroKey<Fault>(FaultUtils.exceptionToFault(nlm.getId(), e, auditSupplementaryData)));
-        	 } catch (SAXException e) {
-				Map<CharSequence, CharSequence> auditSupplementaryData = new HashMap<CharSequence, CharSequence>();
-				auditSupplementaryData.put(FAULT_TEXT, nlm.getText());
-				mos.write(namedOutputFault,
-						new AvroKey<Fault>(FaultUtils.exceptionToFault(nlm.getId(), e, auditSupplementaryData)));
-        	 }
-        }
-    }
-    
-    /* (non-Javadoc)
-	 * @see org.apache.hadoop.mapreduce.Mapper#cleanup(org.apache.hadoop.mapreduce.Mapper.Context)
+	protected void map(AvroKey<DocumentText> key, NullWritable value, Context context)
+			throws IOException, InterruptedException {
+		DocumentText nlm = key.datum();
+		if (!StringUtils.isBlank(nlm.getText())) {
+			final ExtractedDocumentMetadata.Builder output = ExtractedDocumentMetadata.newBuilder();
+			output.setId(nlm.getId());
+			try {
+				// disabling validation
+				SAXParserFactory saxFactory = SAXParserFactory.newInstance();
+				saxFactory.setValidating(false);
+				SAXParser saxParser = saxFactory.newSAXParser();
+				XMLReader reader = saxParser.getXMLReader();
+				reader.setFeature("http://xml.org/sax/features/validation", false);
+				reader.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
+				reader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
+				PmcXmlHandler pmcXmlHandler = new PmcXmlHandler(output);
+				saxParser.parse(new InputSource(new StringReader(nlm.getText().toString())), pmcXmlHandler);
+				mos.write(namedOutputMeta, new AvroKey<ExtractedDocumentMetadata>(output.build()));
+			} catch (ParserConfigurationException e) {
+				handleException(nlm, e);
+			} catch (SAXException e) {
+				handleException(nlm, e);
+			}
+		}
+	}
+
+	/**
+	 * Handles exception by writing it as fault.
+	 * @param documentText
+	 * @param e
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	protected void handleException(DocumentText documentText, Exception e) throws IOException, InterruptedException {
+		Map<CharSequence, CharSequence> auditSupplementaryData = new HashMap<CharSequence, CharSequence>();
+		auditSupplementaryData.put(FAULT_TEXT, documentText.getText());
+		mos.write(namedOutputFault,
+				new AvroKey<Fault>(FaultUtils.exceptionToFault(documentText.getId(), e, auditSupplementaryData)));
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.apache.hadoop.mapreduce.Mapper#cleanup(org.apache.hadoop.mapreduce.
+	 * Mapper.Context)
 	 */
 	@Override
-    public void cleanup(Context context) throws IOException, InterruptedException {
+	public void cleanup(Context context) throws IOException, InterruptedException {
 		log.debug("cleanup: closing multiple outputs...");
-        mos.close();
-        log.debug("cleanup: multiple outputs closed");
-    }
+		mos.close();
+		log.debug("cleanup: multiple outputs closed");
+	}
 }
