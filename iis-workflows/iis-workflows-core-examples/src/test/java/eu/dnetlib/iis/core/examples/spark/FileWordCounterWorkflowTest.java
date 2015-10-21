8c63957879f19baf98e0312c81b193cba416f484
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2015-10-21 11:31:30 +0200

    Closes #53: Write a simple spark example in core-examples Closes #60: Upgrade versions of hadoop libs to v2 (from mr1)

---
+++ iis-workflows/iis-workflows-core-examples/src/test/java/eu/dnetlib/iis/core/examples/spark/FileWordCounterWorkflowTest.java
@@ -0,0 +1,52 @@
+package eu.dnetlib.iis.core.examples.spark;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import eu.dnetlib.iis.IntegrationTest;
+import eu.dnetlib.iis.core.AbstractOozieWorkflowTestCase;
+import eu.dnetlib.iis.core.OozieWorkflowTestConfiguration;
+import eu.dnetlib.iis.core.WorkflowTestResult;
+
+/**
+ * @author Łukasz Dumiszewski
+ */
+@Category(IntegrationTest.class)
+public class FileWordCounterWorkflowTest extends AbstractOozieWorkflowTestCase {
+
+    @Test
+    public void fileWordCounter() throws Exception{
+        
+        
+        // given
+        
+        OozieWorkflowTestConfiguration conf = new OozieWorkflowTestConfiguration();
+        
+        conf.addExpectedOutputFile("output/part-00000");
+        
+        
+        // execute
+        
+        WorkflowTestResult workflowTestResult = testWorkflow("eu/dnetlib/iis/core/examples/spark/file_word_counter", conf);
+        
+        
+        // assert
+        
+        File outputFile = workflowTestResult.getWorkflowOutputFile("output/part-00000"); 
+        
+        String output = FileUtils.readFileToString(outputFile, "UTF-8");
+        
+        
+        assertTrue(output.contains("(universe,2)"));
+        
+        assertTrue(output.contains("(dance,1)"));
+        
+    }
+    
+}
+
