b0ccf5691367ffadfd1fc939b4acf6a98e2d1d40
Krzysztof Mądry <madryk@icm.edu.pl>
2015-10-21 14:25:03 +0200

    Closes #62: Connect to oozie through ssh in integration tests

---
+++ iis-core/src/test/java/eu/dnetlib/iis/core/SshExecUtils.java
@@ -0,0 +1,47 @@
+package eu.dnetlib.iis.core;
+
+import java.io.IOException;
+
+import net.schmizz.sshj.common.IOUtils;
+import net.schmizz.sshj.connection.channel.direct.Session.Command;
+
+/**
+ * Helper util class for working with ssh execution 
+ * 
+ * @author madryk
+ *
+ */
+public class SshExecUtils {
+	
+	
+	//------------------------ CONSTRUCTORS --------------------------
+	
+	private SshExecUtils() {
+		throw new IllegalStateException("Can't instantiate util class " + getClass());
+	}
+	
+	
+	//------------------------ LOGIC --------------------------
+	
+	/**
+	 * Reads output of executed ssh command
+	 */
+	public static String readCommandOutput(Command cmd) {
+		try {
+			return IOUtils.readFully(cmd.getInputStream()).toString();
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to read command output", e);
+		}
+	}
+	
+	/**
+	 * Reads error of executed ssh command
+	 */
+	public static String readCommandError(Command cmd) {
+		try {
+			return IOUtils.readFully(cmd.getErrorStream()).toString();
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to read command error", e);
+		}
+	}
+}
