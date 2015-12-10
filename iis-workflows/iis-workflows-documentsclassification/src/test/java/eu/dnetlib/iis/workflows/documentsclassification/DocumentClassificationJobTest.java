865d276efb625634748bcc942d5061307b2e9597
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2015-12-10 14:32:35 +0100

    Closes #113: Rewrite document-classification process part to Spark

--- iis-workflows/iis-workflows-documentsclassification/src/test/java/eu/dnetlib/iis/workflows/documentsclassification/DocumentClassificationJobTest.java
+++ iis-workflows/iis-workflows-documentsclassification/src/test/java/eu/dnetlib/iis/workflows/documentsclassification/DocumentClassificationJobTest.java
@@ -2,11 +2,18 @@ package eu.dnetlib.iis.workflows.documentsclassification;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import pl.edu.icm.sparkutils.test.SparkJob;
 import pl.edu.icm.sparkutils.test.SparkJobBuilder;
@@ -17,7 +24,7 @@ import com.google.common.io.Files;
 import eu.dnetlib.iis.core.common.AvroAssertTestUtil;
 import eu.dnetlib.iis.core.common.AvroTestUtils;
 import eu.dnetlib.iis.core.common.JsonAvroTestUtils;
-import eu.dnetlib.iis.documentsclassification.schemas.DocumentMetadata;
+import eu.dnetlib.iis.documentsclassification.schemas.DocumentToDocumentClasses;
 import eu.dnetlib.iis.transformers.metadatamerger.schemas.ExtractedDocumentMetadataMergedWithOriginal;
 
 /**
@@ -26,17 +33,36 @@ import eu.dnetlib.iis.transformers.metadatamerger.schemas.ExtractedDocumentMetad
 
 public class DocumentClassificationJobTest {
 
+    private static Logger log = LoggerFactory.getLogger(DocumentClassificationJobTest.class);
+    
     
     private SparkJobExecutor executor = new SparkJobExecutor();
     
     private File workingDir;
     
+    private String inputDirPath;
     
+    private String outputDirPath;
+    
+    private static String scriptDirPath;
+    
+    
+    @BeforeClass
+    public static void beforeClass() throws IOException {
+
+        scriptDirPath = DocumentClassificationJobTest.class.getResource("/eu/dnetlib/iis/workflows/documentsclassification/oozie_app/lib/scripts").getPath();
+        copyMadis(scriptDirPath + "/madis");
+
+    }
     
     @Before
     public void before() {
         
         workingDir = Files.createTempDir();
+        
+        inputDirPath = workingDir + "/document_classification/input";
+        outputDirPath = workingDir + "/document_classification/output";
+        
         
     }
     
@@ -57,11 +83,8 @@ public class DocumentClassificationJobTest {
         
         // given
         
-        String inputDirPath = workingDir + "/document_classification/input";
-        String outputDirPath = workingDir + "/document_classification/output";
-        
-        String jsonInputFile = "src/test/resources/eu/dnetlib/iis/workflows/documentsclassification/data/input/spark_job_test_documents.json";
-        String jsonOutputFile = "src/test/resources/eu/dnetlib/iis/workflows/documentsclassification/data/expected_output/spark_job_test_dc_metadata.json";
+        String jsonInputFile = "src/test/resources/eu/dnetlib/iis/workflows/documentsclassification/data/input/few_documents.json";
+        String jsonOutputFile = "src/test/resources/eu/dnetlib/iis/workflows/documentsclassification/data/expected_output/few_document_to_document_classes.json";
         
         
         AvroTestUtils.createLocalAvroDataStore(
@@ -69,8 +92,78 @@ public class DocumentClassificationJobTest {
                 inputDirPath);
         
         
+        // execute & assert
+        
+        executeJobAndAssert(jsonOutputFile);
+    }
+        
+    
+    @Test
+    public void documentClassificationJob_EMPTY_ABSTRACT() throws IOException {
         
         
+        // given
+        
+        String jsonInputFile = "src/test/resources/eu/dnetlib/iis/workflows/documentsclassification/data/input/documents_with_empty_abstract.json";
+        String jsonOutputFile = "src/test/resources/eu/dnetlib/iis/workflows/documentsclassification/data/expected_output/empty.json";
+        
+        
+        AvroTestUtils.createLocalAvroDataStore(
+                JsonAvroTestUtils.readJsonDataStore(jsonInputFile, ExtractedDocumentMetadataMergedWithOriginal.class),
+                inputDirPath);
+        
+        
+        // execute & assert
+        
+        executeJobAndAssert(jsonOutputFile);
+    }
+    
+    
+    @Test
+    public void documentClassificationJob_NULL_TAXONOMY() throws IOException {
+        
+        
+        // given
+        
+        String jsonInputFile = "src/test/resources/eu/dnetlib/iis/workflows/documentsclassification/data/input/documents_with_null_taxonomy.json";
+        String jsonOutputFile = "src/test/resources/eu/dnetlib/iis/workflows/documentsclassification/data/expected_output/document_to_document_classes_null_taxonomy.json";
+        
+        
+        AvroTestUtils.createLocalAvroDataStore(
+                JsonAvroTestUtils.readJsonDataStore(jsonInputFile, ExtractedDocumentMetadataMergedWithOriginal.class),
+                inputDirPath);
+        
+        
+        // execute & assert
+        
+        executeJobAndAssert(jsonOutputFile);
+    }
+    
+    
+    @Test
+    public void documentClassificationJob_EMPTY() throws IOException {
+        
+        // given
+        
+        String avroInputFile = "src/test/resources/eu/dnetlib/iis/workflows/documentsclassification/data/input/empty.avro";
+        String jsonOutputFile = "src/test/resources/eu/dnetlib/iis/workflows/documentsclassification/data/expected_output/empty.json";
+        
+        
+        FileUtils.copyFileToDirectory(new File(avroInputFile), new File(inputDirPath));
+        
+
+        // execute & assert
+        
+        executeJobAndAssert(jsonOutputFile);
+    }
+    
+
+    
+    //------------------------ PRIVATE --------------------------
+    
+    
+    private void executeJobAndAssert(String jsonOutputFile) throws IOException {
+
         SparkJob sparkJob = SparkJobBuilder
                                            .create()
                                            
@@ -79,7 +172,7 @@ public class DocumentClassificationJobTest {
                                            .setMainClass(DocumentClassificationJob.class)
                                            .addArg("-inputAvroPath", inputDirPath)
                                            .addArg("-outputAvroPath", outputDirPath)
-                                           
+                                           .addArg("-scriptDirPath", scriptDirPath)
                                            .build();
         
         
@@ -91,7 +184,34 @@ public class DocumentClassificationJobTest {
         
         // assert
         
-        AvroAssertTestUtil.assertEqualsWithJson(outputDirPath, jsonOutputFile, DocumentMetadata.class);
+        AvroAssertTestUtil.assertEqualsWithJsonIgnoreOrder(outputDirPath, jsonOutputFile, DocumentToDocumentClasses.class);
     }
+
+
+    
+    private static void copyMadis(String destDirectory) throws IOException {
         
+        String directoryToScan = "/eu/dnetlib/iis/3rdparty/scripts/madis/";
+        
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources(ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + directoryToScan + "**");
+        
+        for (Resource resource : resources) {
+            
+            if (resource.exists() & resource.isReadable() & (resource.contentLength() > 0 || resource.getFilename().equals("__init__.py"))) {
+                
+                URL url = resource.getURL();
+                String urlString = url.toExternalForm();
+                String targetName = urlString.substring(urlString.indexOf(directoryToScan)+directoryToScan.length());
+                File destination = new File(destDirectory, targetName);
+                FileUtils.copyURLToFile(url, destination);
+            
+                log.debug("Copied {} to {}", url, destination.getAbsolutePath());
+            
+            } else {
+                log.debug("Did not copy, seems to be directory: {} ", resource.getDescription());
+            
+            }
+        }
+    }
 }
