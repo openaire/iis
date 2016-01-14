82afdc11a51b91cab6a827390b8894e4d84c7ed2
Marek Horst <mhorst@icm.edu.pl>
2016-01-14 18:18:36 +0100

    Closes #176: supplement integration test suite

--- iis-workflows/iis-workflows-top/src/test/java/eu/dnetlib/iis/workflows/top/WorkflowTest.java
+++ iis-workflows/iis-workflows-top/src/test/java/eu/dnetlib/iis/workflows/top/WorkflowTest.java
@@ -1,5 +1,6 @@
 package eu.dnetlib.iis.workflows.top;
 
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import eu.dnetlib.iis.IntegrationTest;
@@ -15,16 +16,14 @@ import eu.dnetlib.iis.core.OozieWorkflowTestConfiguration;
 @Category(IntegrationTest.class)
 public class WorkflowTest extends AbstractOozieWorkflowTestCase {
 
-//	@Test
-//	disabled, introducing definition in deploy.info file causing jenkins job creation
+	@Test
 	public void testIntegrationImportWorkflow() throws Exception {
 		OozieWorkflowTestConfiguration wfConf = new OozieWorkflowTestConfiguration();
 		wfConf.setTimeoutInSeconds(720);
 		testWorkflow("eu/dnetlib/iis/workflows/top/integration/import", wfConf);
 	}
 
-//	@Test
-//	disabled, introducing definition in deploy.info file causing jenkins job creation
+	@Test
 	public void testIntegrationMainChainWorkflow() throws Exception {
 		// this is long running test, so we need to increate timeout
 		OozieWorkflowTestConfiguration wfConf = new OozieWorkflowTestConfiguration();
