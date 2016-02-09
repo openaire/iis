672d3b15dcedf48ded3146682fa105203a1bd022
Krzysztof Mądry <madryk@icm.edu.pl>
2016-02-09 11:21:53 +0100

    Closes #184: CitationMatching workflow using spark

---
+++ iis-workflows/iis-workflows-citationmatching/src/main/java/eu/dnetlib/iis/workflows/citationmatching/converter/DocumentMetadataToMatchableConverter.java
@@ -0,0 +1,41 @@
+package eu.dnetlib.iis.workflows.citationmatching.converter;
+
+import static eu.dnetlib.iis.workflows.citationmatching.converter.BasicMetadataDataExtractionUtil.extractAuthors;
+import static eu.dnetlib.iis.workflows.citationmatching.converter.BasicMetadataDataExtractionUtil.extractJournal;
+import static eu.dnetlib.iis.workflows.citationmatching.converter.BasicMetadataDataExtractionUtil.extractPages;
+import static eu.dnetlib.iis.workflows.citationmatching.converter.BasicMetadataDataExtractionUtil.extractTitle;
+import static eu.dnetlib.iis.workflows.citationmatching.converter.BasicMetadataDataExtractionUtil.extractYear;
+
+import java.io.Serializable;
+
+import eu.dnetlib.iis.citationmatching.schemas.BasicMetadata;
+import eu.dnetlib.iis.citationmatching.schemas.DocumentMetadata;
+import pl.edu.icm.coansys.citations.data.MatchableEntity;
+
+/**
+ * Converter of {@link DocumentMetadata} object to {@link MatchableEntity} object
+ * 
+ * @author madryk
+ */
+public class DocumentMetadataToMatchableConverter implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    //------------------------ LOGIC --------------------------
+
+    /**
+     * Converts {@link DocumentMetadata} to {@link MatchableEntity}.
+     * As {@link MatchableEntity#id()} it uses documentId parameter.
+     */
+    public MatchableEntity convertToMatchableEntity(String documentId, DocumentMetadata docMetadata) {
+
+        BasicMetadata metadata = docMetadata.getBasicMetadata();
+
+        MatchableEntity entity = MatchableEntity.fromParameters(documentId, 
+                extractAuthors(metadata), extractJournal(metadata), extractTitle(metadata), 
+                extractPages(metadata), extractYear(metadata), null);
+
+        return entity;
+    }
+}
---
+++ iis-workflows/iis-workflows-citationmatching/src/main/java/eu/dnetlib/iis/workflows/citationmatching/converter/MatchedCitationToCitationConverter.java
@@ -0,0 +1,42 @@
+package eu.dnetlib.iis.workflows.citationmatching.converter;
+
+import java.io.Serializable;
+
+import eu.dnetlib.iis.citationmatching.schemas.Citation;
+import eu.dnetlib.iis.workflows.citationmatching.converter.entity_id.CitEntityId;
+import eu.dnetlib.iis.workflows.citationmatching.converter.entity_id.DocEntityId;
+import pl.edu.icm.coansys.citations.data.IdWithSimilarity;
+import pl.edu.icm.coansys.citations.data.MatchableEntity;
+
+/**
+ * Converter of {@link MatchableEntity} and {@link IdWithSimilarity} objects
+ * to {@link Citation} object
+ * 
+ * @author madryk
+ */
+public class MatchedCitationToCitationConverter implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    //------------------------ LOGIC --------------------------
+
+    /**
+     * Returns {@link Citation} converted from {@link MatchableEntity} and {@link IdWithSimilarity} objects
+     */
+    public Citation convertToCitation(MatchableEntity citationEntity, IdWithSimilarity docIdWithSimilarity) {
+
+        CitEntityId citEntityId = CitEntityId.parseFrom(citationEntity.id());
+        DocEntityId docEntityId = DocEntityId.parseFrom(docIdWithSimilarity.getId());
+
+        Citation cit = new Citation();
+
+        cit.setSourceDocumentId(citEntityId.getSourceDocumentId());
+        cit.setPosition(citEntityId.getPosition());
+        cit.setDestinationDocumentId(docEntityId.getDocumentId());
+        cit.setConfidenceLevel((float)docIdWithSimilarity.getSimilarity());
+
+        return cit;
+    }
+
+}
