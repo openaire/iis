f2f936ac22477df3ab7a88c92f403b551c123ae1
Michał Politowski <mpol@icm.edu.pl>
2016-04-11 16:18:44 +0200

    Restore a change accidentally removed during a merge from master
    `classify_documents.sh` was changed in the cdh5 branch. Last merge from master
    accidentally reverted that change, causing errors.

--- iis-wf/iis-wf-documentsclassification/src/main/resources/eu/dnetlib/iis/wf/documentsclassification/oozie_app/lib/scripts/classify_documents.sh
+++ iis-wf/iis-wf-documentsclassification/src/main/resources/eu/dnetlib/iis/wf/documentsclassification/oozie_app/lib/scripts/classify_documents.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -o pipefail
-scripts/madis/mexec.py -d scripts/taxonomies.db -f scripts/classify.sql | scripts/complete_taxonomies.py
+python $1/madis/mexec.py -d $1/taxonomies.db -f $1/classify.sql | python $1/complete_taxonomies.py
