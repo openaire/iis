58786ec7833232b48ece8ad4ebe26c96bf018be4
Marek Horst <mhorst@icm.edu.pl>
2016-04-14 17:51:56 +0200

    Closes #256: Write workflow integration tests for wf-ingest module

---
+++ iis-wf/iis-wf-ingest/src/test/java/eu/dnetlib/iis/wf/ingest/webcrawl/fundings/WorkflowTest.java
@@ -0,0 +1,30 @@
+package eu.dnetlib.iis.wf.ingest.webcrawl.fundings;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import eu.dnetlib.iis.common.AbstractOozieWorkflowTestCase;
+import eu.dnetlib.iis.common.IntegrationTest;
+
+/**
+ * @author mhorst
+ *
+ */
+@Category(IntegrationTest.class)
+public class WorkflowTest extends AbstractOozieWorkflowTestCase {
+
+	@Test
+	public void testIngestWebcrawlFundings() throws Exception {
+    	testWorkflow("eu/dnetlib/iis/wf/ingest/webcrawl/fundings/sampledataproducer");
+	}
+	
+	@Test
+	public void testIngestWebcrawlFundingsFromBrokenXml() throws Exception {
+    	testWorkflow("eu/dnetlib/iis/wf/ingest/webcrawl/fundings/brokenxml");
+	}
+	
+	@Test
+	public void testIngestWebcrawlFundingsFromNullText() throws Exception {
+    	testWorkflow("eu/dnetlib/iis/wf/ingest/webcrawl/fundings/nulltext");
+	}
+}
