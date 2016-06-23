6db44f4b04da896391c3d3e6d14d5cf5e668df55
Marek Horst <mhorst@icm.edu.pl>
2016-06-23 22:21:10 +0200

    Closes #514: Separate content retrieval from metadata extraction
    - indentation fix

--- iis-wf/iis-wf-import/src/main/java/eu/dnetlib/iis/wf/importer/content/DocumentContentUrlBasedImporterMapper.java
+++ iis-wf/iis-wf-import/src/main/java/eu/dnetlib/iis/wf/importer/content/DocumentContentUrlBasedImporterMapper.java
@@ -22,75 +22,69 @@ import eu.dnetlib.iis.wf.importer.content.appover.SizeLimitContentApprover;
 
 /**
  * {@link DocumentContentUrl} based importer producing {@link DocumentContent} output.
+ * 
  * @author mhorst
  *
  */
-public class DocumentContentUrlBasedImporterMapper extends Mapper<AvroKey<DocumentContentUrl>, NullWritable, AvroKey<DocumentContent>, NullWritable> {
+public class DocumentContentUrlBasedImporterMapper
+        extends Mapper<AvroKey<DocumentContentUrl>, NullWritable, AvroKey<DocumentContent>, NullWritable> {
 
-	
-	private final Logger log = Logger.getLogger(DocumentContentUrlBasedImporterMapper.class);
-	
-	/**
-	 * Content approver module.
-	 */
-	private ContentApprover contentApprover;
-	
-	/**
-	 * Connection timeout.
-	 */
-	private int connectionTimeout;
-	
-	/**
-	 * Read timeout.
-	 */
-	private int readTimeout;
-	
-	@Override
-	protected void setup(Context context) throws IOException,
-			InterruptedException {
-//		connection and approver related parameters
-		this.connectionTimeout = context.getConfiguration().getInt(
-									IMPORT_CONTENT_CONNECTION_TIMEOUT, 60000);
-		this.readTimeout = context.getConfiguration().getInt(
-									IMPORT_CONTENT_READ_TIMEOUT, 60000);
+    private final Logger log = Logger.getLogger(DocumentContentUrlBasedImporterMapper.class);
+
+    /**
+     * Content approver module.
+     */
+    private ContentApprover contentApprover;
+
+    /**
+     * Connection timeout.
+     */
+    private int connectionTimeout;
+
+    /**
+     * Read timeout.
+     */
+    private int readTimeout;
+    
+    //------------------------ LOGIC --------------------------
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        // connection and approver related parameters
+        this.connectionTimeout = context.getConfiguration().getInt(IMPORT_CONTENT_CONNECTION_TIMEOUT, 60000);
+        this.readTimeout = context.getConfiguration().getInt(IMPORT_CONTENT_READ_TIMEOUT, 60000);
         String maxFileSizeMBStr = WorkflowRuntimeParameters.getParamValue(
                 ImportWorkflowRuntimeParameters.IMPORT_CONTENT_MAX_FILE_SIZE_MB, context.getConfiguration());
         if (maxFileSizeMBStr != null) {
-            this.contentApprover = new ComplexContentApprover(
-                    new PDFHeaderBasedContentApprover(),
+            this.contentApprover = new ComplexContentApprover(new PDFHeaderBasedContentApprover(),
                     new SizeLimitContentApprover(Integer.valueOf(maxFileSizeMBStr)));
-		} else {
-			this.contentApprover = new PDFHeaderBasedContentApprover();
-		}
-	}
-	
-	@Override
-	protected void map(AvroKey<DocumentContentUrl> key, NullWritable value,
-			Context context) throws IOException, InterruptedException {
-		DocumentContentUrl docUrl = key.datum();
-		long startTimeContent = System.currentTimeMillis();
-		log.info("starting content retrieval for id: " + docUrl.getId() + 
-                ", location: " + docUrl.getUrl() + " and size [kB]: " + docUrl.getContentSizeKB());
-		byte[] content = ObjectStoreContentProviderUtils.getContentFromURL(
-				docUrl.getUrl().toString(), 
-				this.connectionTimeout, this.readTimeout);
-		log.info("content retrieval for id: " + docUrl.getId() + " took: " +
-				(System.currentTimeMillis()-startTimeContent) + " ms, got content: " +
-				(content!=null && content.length>0));
-		if (contentApprover.approve(content)) {
-			DocumentContent.Builder documentContentBuilder = DocumentContent.newBuilder();
-			documentContentBuilder.setId(docUrl.getId());
-			if (content!=null) {
-				documentContentBuilder.setPdf(ByteBuffer.wrap(
-						content));
-			}
-			context.write(
-					new AvroKey<DocumentContent>(documentContentBuilder.build()), 
-					NullWritable.get());
-		} else {
-			log.info("content " + docUrl.getId() + " not approved " +
-					"for location: " + docUrl.getUrl() + " and size [kB]: " + docUrl.getContentSizeKB());
-		}
-		
-	}
+        } else {
+            this.contentApprover = new PDFHeaderBasedContentApprover();
+        }
+    }
+
+    @Override
+    protected void map(AvroKey<DocumentContentUrl> key, NullWritable value, Context context)
+            throws IOException, InterruptedException {
+        DocumentContentUrl docUrl = key.datum();
+        long startTimeContent = System.currentTimeMillis();
+        log.info("starting content retrieval for id: " + docUrl.getId() + ", location: " + docUrl.getUrl()
+                + " and size [kB]: " + docUrl.getContentSizeKB());
+        byte[] content = ObjectStoreContentProviderUtils.getContentFromURL(docUrl.getUrl().toString(),
+                this.connectionTimeout, this.readTimeout);
+        log.info("content retrieval for id: " + docUrl.getId() + " took: "
+                + (System.currentTimeMillis() - startTimeContent) + " ms, got content: "
+                + (content != null && content.length > 0));
+        if (contentApprover.approve(content)) {
+            DocumentContent.Builder documentContentBuilder = DocumentContent.newBuilder();
+            documentContentBuilder.setId(docUrl.getId());
+            if (content != null) {
+                documentContentBuilder.setPdf(ByteBuffer.wrap(content));
+            }
+            context.write(new AvroKey<DocumentContent>(documentContentBuilder.build()), NullWritable.get());
+        } else {
+            log.info("content " + docUrl.getId() + " not approved " + "for location: " + docUrl.getUrl()
+                    + " and size [kB]: " + docUrl.getContentSizeKB());
+        }
+    }
 }
