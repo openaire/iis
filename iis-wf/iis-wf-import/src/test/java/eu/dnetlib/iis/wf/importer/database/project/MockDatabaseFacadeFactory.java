10646d1424a92db7db4ec982b8c821a58074096f
Marek Horst <mhorst@icm.edu.pl>
2016-07-28 17:50:32 +0200

    Closes #583: Write project database importer workflow integration test

---
+++ iis-wf/iis-wf-import/src/test/java/eu/dnetlib/iis/wf/importer/database/project/MockDatabaseFacadeFactory.java
@@ -0,0 +1,47 @@
+package eu.dnetlib.iis.wf.importer.database.project;
+
+import java.util.Map;
+
+import eu.dnetlib.iis.wf.importer.StaticResourcesProvider;
+import eu.dnetlib.iis.wf.importer.facade.DatabaseFacade;
+import eu.dnetlib.iis.wf.importer.facade.ServiceFacadeException;
+import eu.dnetlib.iis.wf.importer.facade.ServiceFacadeFactory;
+
+/**
+ * Factory for building Database facade mocks.
+ * @author mhorst
+ *
+ */
+public class MockDatabaseFacadeFactory implements ServiceFacadeFactory<DatabaseFacade> {
+
+    
+    //------------------------ LOGIC --------------------------
+    
+    @Override
+    public DatabaseFacade instantiate(Map<String, String> parameters) {
+        return new MockDatabaseFacade();
+    }
+    
+    //--------------- INNER CLASS -------------------
+
+    /**
+     * ISLookup mock providing static concept profiles.
+     *
+     */
+    private static class MockDatabaseFacade implements DatabaseFacade {
+        
+        private static final String[] profileLocations = new String[] {
+                "/eu/dnetlib/iis/wf/importer/database/project/data/input/project_with_optional.xml",
+                "/eu/dnetlib/iis/wf/importer/database/project/data/input/project_no_optional_no_fundingtree.xml", 
+                "/eu/dnetlib/iis/wf/importer/database/project/data/input/project_null_acronym_null_optional.xml" };
+
+        //------------------------ LOGIC --------------------------
+
+        @Override
+        public Iterable<String> searchSQL(String databaseName, String query) throws ServiceFacadeException {
+            return StaticResourcesProvider.getResources(profileLocations);
+        }
+
+    }
+}
+
