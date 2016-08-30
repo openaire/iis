25a4cf0bc6b3f3e99ef7989e5d607b2b6f367790
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2016-08-30 13:33:48 +0200

    test name change

--- iis-wf/iis-wf-transformers/src/test/java/eu/dnetlib/iis/wf/transformer/export/identifier/documenttodataset/DocDatasetToMdStoreWorkflowTest.java
+++ iis-wf/iis-wf-transformers/src/test/java/eu/dnetlib/iis/wf/transformer/export/identifier/documenttodataset/DocDatasetToMdStoreWorkflowTest.java
@@ -11,7 +11,7 @@ import eu.dnetlib.iis.common.OozieWorkflowTestConfiguration;
 * @author Łukasz Dumiszewski
 */
 @Category(IntegrationTest.class)
-public class DocDatasetToMdStoreWorkflow extends AbstractOozieWorkflowTestCase {
+public class DocDatasetToMdStoreWorkflowTest extends AbstractOozieWorkflowTestCase {
     
     @Test
     public void testWorkflow() throws Exception {
