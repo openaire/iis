ce83f342e11f7ad22850dc163cf09d9b9263e87a
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2016-08-30 12:57:47 +0200

    Closes #645: Correct the pig script and write an int test for transformers/export/identifier/documenttodataset workflow

---
+++ iis-wf/iis-wf-transformers/src/test/java/eu/dnetlib/iis/wf/transformer/export/identifier/documenttodataset/DocDatasetToMdStoreWorkflow.java
@@ -0,0 +1,22 @@
+package eu.dnetlib.iis.wf.transformer.export.identifier.documenttodataset;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import eu.dnetlib.iis.common.AbstractOozieWorkflowTestCase;
+import eu.dnetlib.iis.common.IntegrationTest;
+import eu.dnetlib.iis.common.OozieWorkflowTestConfiguration;
+
+/**
+* @author Łukasz Dumiszewski
+*/
+@Category(IntegrationTest.class)
+public class DocDatasetToMdStoreWorkflow extends AbstractOozieWorkflowTestCase {
+    
+    @Test
+    public void testWorkflow() throws Exception {
+        OozieWorkflowTestConfiguration wf = new OozieWorkflowTestConfiguration();
+        testWorkflow("eu/dnetlib/iis/wf/transformers/export/identifier/documenttodataset/test", wf);
+    }
+    
+}
