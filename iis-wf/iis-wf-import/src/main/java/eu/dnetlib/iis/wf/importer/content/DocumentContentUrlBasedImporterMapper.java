448c1a29b6d5ad0fa382239bd98582ce5ffa61a8
Marek Horst <mhorst@icm.edu.pl>
2016-10-21 11:02:13 +0200

    Closes #696: Check content size before obtaining whole document

--- iis-wf/iis-wf-import/src/main/java/eu/dnetlib/iis/wf/importer/content/DocumentContentUrlBasedImporterMapper.java
+++ iis-wf/iis-wf-import/src/main/java/eu/dnetlib/iis/wf/importer/content/DocumentContentUrlBasedImporterMapper.java
@@ -6,7 +6,6 @@ import static eu.dnetlib.iis.wf.importer.ImportWorkflowRuntimeParameters.IMPORT_
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.List;
 
 import org.apache.avro.mapred.AvroKey;
 import org.apache.hadoop.io.NullWritable;
@@ -14,16 +13,12 @@ import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.log4j.Logger;
 
-import com.google.common.collect.Lists;
-
 import eu.dnetlib.iis.common.WorkflowRuntimeParameters;
 import eu.dnetlib.iis.importer.auxiliary.schemas.DocumentContentUrl;
 import eu.dnetlib.iis.importer.schemas.DocumentContent;
-import eu.dnetlib.iis.wf.importer.content.approver.ComplexContentApprover;
 import eu.dnetlib.iis.wf.importer.content.approver.ContentApprover;
 import eu.dnetlib.iis.wf.importer.content.approver.InvalidCountableContentApproverWrapper;
 import eu.dnetlib.iis.wf.importer.content.approver.PDFHeaderBasedContentApprover;
-import eu.dnetlib.iis.wf.importer.content.approver.SizeLimitContentApprover;
 
 /**
  * {@link DocumentContentUrl} based importer producing {@link DocumentContent} output.
@@ -52,6 +47,16 @@ public class DocumentContentUrlBasedImporterMapper
     private int readTimeout;
     
     /**
+     * Maximum allowed file size expressed in KB.
+     */
+    private long maxFileSizeKB = Long.MAX_VALUE;
+    
+    /**
+     * Counter for the records with content size exceeded.
+     */
+    private Counter sizeExceededCounter;
+    
+    /**
      * Hadoop counters enum of invalid records 
      */
     public static enum InvalidRecordCounters {
@@ -66,45 +71,48 @@ public class DocumentContentUrlBasedImporterMapper
         // connection and approver related parameters
         this.connectionTimeout = context.getConfiguration().getInt(IMPORT_CONTENT_CONNECTION_TIMEOUT, 60000);
         this.readTimeout = context.getConfiguration().getInt(IMPORT_CONTENT_READ_TIMEOUT, 60000);
-        Integer maxFileSizeMB = WorkflowRuntimeParameters.getIntegerParamValue(
-                IMPORT_CONTENT_MAX_FILE_SIZE_MB, context.getConfiguration());
         
         Counter invalidPdfCounter = context.getCounter(InvalidRecordCounters.INVALID_PDF_HEADER);
-        Counter sizeExceededCounter = context.getCounter(InvalidRecordCounters.SIZE_EXCEEDED);
         invalidPdfCounter.setValue(0);
-        sizeExceededCounter.setValue(0);
-        
-        List<ContentApprover> contentApprovers = Lists.newArrayList();
-        contentApprovers.add(new InvalidCountableContentApproverWrapper(new PDFHeaderBasedContentApprover(), invalidPdfCounter));
-        
+        this.contentApprover = new InvalidCountableContentApproverWrapper(new PDFHeaderBasedContentApprover(), invalidPdfCounter);
+
+        this.sizeExceededCounter = context.getCounter(InvalidRecordCounters.SIZE_EXCEEDED);
+        this.sizeExceededCounter.setValue(0);
+        Integer maxFileSizeMB = WorkflowRuntimeParameters.getIntegerParamValue(
+                IMPORT_CONTENT_MAX_FILE_SIZE_MB, context.getConfiguration());
         if (maxFileSizeMB != null) {
-            contentApprovers.add(new InvalidCountableContentApproverWrapper(new SizeLimitContentApprover(maxFileSizeMB), sizeExceededCounter));
+            this.maxFileSizeKB = 1024l * maxFileSizeMB;
         }
-        this.contentApprover = new ComplexContentApprover(contentApprovers);
     }
 
     @Override
     protected void map(AvroKey<DocumentContentUrl> key, NullWritable value, Context context)
             throws IOException, InterruptedException {
         DocumentContentUrl docUrl = key.datum();
-        long startTimeContent = System.currentTimeMillis();
-        log.info("starting content retrieval for id: " + docUrl.getId() + ", location: " + docUrl.getUrl()
-                + " and size [kB]: " + docUrl.getContentSizeKB());
-        byte[] content = ObjectStoreContentProviderUtils.getContentFromURL(docUrl.getUrl().toString(),
-                this.connectionTimeout, this.readTimeout);
-        log.info("content retrieval for id: " + docUrl.getId() + " took: "
-                + (System.currentTimeMillis() - startTimeContent) + " ms, got content: "
-                + (content != null && content.length > 0));
-        if (contentApprover.approve(content)) {
-            DocumentContent.Builder documentContentBuilder = DocumentContent.newBuilder();
-            documentContentBuilder.setId(docUrl.getId());
-            if (content != null) {
-                documentContentBuilder.setPdf(ByteBuffer.wrap(content));
-            }
-            context.write(new AvroKey<DocumentContent>(documentContentBuilder.build()), NullWritable.get());
-        } else {
-            log.info("content " + docUrl.getId() + " not approved " + "for location: " + docUrl.getUrl()
+        if (docUrl.getContentSizeKB() <= maxFileSizeKB) {
+            long startTimeContent = System.currentTimeMillis();
+            log.info("starting content retrieval for id: " + docUrl.getId() + ", location: " + docUrl.getUrl()
                     + " and size [kB]: " + docUrl.getContentSizeKB());
+            byte[] content = ObjectStoreContentProviderUtils.getContentFromURL(docUrl.getUrl().toString(),
+                    this.connectionTimeout, this.readTimeout);
+            log.info("content retrieval for id: " + docUrl.getId() + " took: "
+                    + (System.currentTimeMillis() - startTimeContent) + " ms, got content: "
+                    + (content != null && content.length > 0));
+            if (contentApprover.approve(content)) {
+                DocumentContent.Builder documentContentBuilder = DocumentContent.newBuilder();
+                documentContentBuilder.setId(docUrl.getId());
+                if (content != null) {
+                    documentContentBuilder.setPdf(ByteBuffer.wrap(content));
+                }
+                context.write(new AvroKey<DocumentContent>(documentContentBuilder.build()), NullWritable.get());
+            } else {
+                log.info("content " + docUrl.getId() + " not approved, location: " + docUrl.getUrl());
+            }
+        } else {
+            this.sizeExceededCounter.increment(1);
+            log.info("content " + docUrl.getId() + " discarded for location: " + docUrl.getUrl()
+            + " and size [kB]: " + docUrl.getContentSizeKB() + ", size limit: " + maxFileSizeKB + " exceeded!");
         }
+
     }
 }
