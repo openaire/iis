e20ebf50d49a7b9c557376d8c919d19b19b5478b
Marek Horst <mhorst@icm.edu.pl>
2017-03-10 11:44:08 +0100

    Closes #760: Update EGI reference extraction VOs
    redmine: #2384

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/researchinitiative/main/oozie_app/lib/scripts/extract.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/researchinitiative/main/oozie_app/lib/scripts/extract.sql
@@ -1,23 +1,24 @@
 hidden var 'a' from (select jmergeregexp(jgroup(c1)) from (select * from egidigrams order by length(C1) desc)) ;
 hidden var 'pos' from (select jmergeregexp(jgroup(c1)) from (select * from positives order by length(C1) desc)) ; 
 
-select jdict('documentId', doi, 'conceptId',id,'confidenceLevel', conf) from (
-    select distinct doi,id,1 as conf from (
-        select  doi, c1, id,
-	            regexpcountwords(c2,keywords(j2s(prevpack,middle,nextpack)))*0.4 + 
-                    regexpcountwords(var('pos'),keywords(j2s(prevpack,middle,nextpack)))*0.4 as conf,   
-                prevpack, middle, nextpack  
-        from (
-            select doi, id, digrams as c2, lower(prev) as prevpack,lower(middle) as middle, middle as match, lower(next) as nextpack, c1
-            from (
+
+
+create temp table results as 
+select distinct doi,name,id,j2s(prevpack,middle,nextpack) as context,conf1,conf2 from (select  doi,c1,name,  id,             regexpcountwords(c2,keywords(j2s(prevpack,middle,nextpack))) as conf1,regexpcountuniquematches(var('pos'),keywords(j2s(prevpack,middle,nextpack))) as conf2,                    prevpack, middle, nextpack           
+        from (             
+            select doi, c1 as name,id, digrams as c2, lower(prev) as prevpack,lower(middle) as middle, middle as match, lower(next) as nextpack, c1 
+            from (                 
                 select * from (
-                    setschema 'doi, prev, middle, next' select  c1 as doi, textwindow2s(filterstopwords(lower(regexpr('\n',c2,' '))),10,1,3,var('a')) from  (
+                    setschema 'doi, prev, middle, next' select c1 as doi, textwindow2s(filterstopwords(lower(regexpr('\n',c2,' '))),10,3,3,var('a')) from(          
                         select * from (
-                            setschema 'c1,c2' select jsonpath(c1, '$.id', '$.text') from stdinput()
-                        ) where c2 is not null
-                    )
-                ), egidigrams where  regexprmatches(c1,middle)
-            )
-          )
-    ) where conf>0
-);
+                           setschema 'c1,c2' select jsonpath(c1, '$.id', '$.text') from stdinput()                     
+                        ) where c2 is not null 
+                    )                 
+                ), egidigrams where regexprmatches(c1,middle)             
+                 )           
+             )     
+        ) where conf1+conf2>0;
+
+
+select jdict('documentId', doi, 'conceptId',id,'confidenceLevel', 0.8) from (
+select distinct doi,id from results where not regexprmatches("(?:\(2\d{3}\))|\[\d+\]|(?:et\sal)",context)) ;
