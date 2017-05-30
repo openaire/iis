a1b46e7c0c3bd03b12729ca78c5475ba106f3808
Yannis Foufoulas <openaire@dl126.madgik.di.uoa.gr>
2017-05-30 18:02:01 +0300

    fix print errors - software enrichment

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/softwareurl/main/oozie_app/lib/scripts/software.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/softwareurl/main/oozie_app/lib/scripts/software.sql
@@ -1,16 +1,47 @@
-select jdict('documentId', id, 'softwareUrl', match,'repositoryName',repo,'confidenceLevel',0.8) from (
+PRAGMA temp_store_directory = '.';
+
+create temp table pubs as setschema 'c1,c2' select jsonpath(c1, '$.id', '$.text') from stdinput();
+
+select
+jdict('documentId', id, 'softwareURL', match,'repositoryName',comprspaces(regexpr("\n",repo," ")),'softwareTitle',
+comprspaces(regexpr("\n",title," ")),'softwareDescription',comprspaces(regexpr("\n",description," ")),'softwarePageURL',url,'confidenceLevel',0.8) from 
+(select id,match,repo,case when title is null then "" else title end as title,case when description is null then "" else description end as description,url from (select id, match, repo,
+case
+        when repo='GitHub' then regexpr('itemprop="name">.+>(.+)</a>', data)
+        when repo='Bitbucket' then regexpr('bitbucket.org/[\w-]+/([\w-]+)/?', match)
+        when repo='SourceForge' then regexpr('<meta property="og:title" content="(.+)"/>', data)
+        when repo='Google Code' then
+                case
+                        when regexprmatches('<meta name="hostname" content="github.com">', data) then regexpr('<strong itemprop="name">.+>(.+)</a>', data)
+                        else regexpr('itemprop="name">(.+)</span>', data)
+                end
+end as title,
+case
+        when repo='GitHub' then regexpr('itemprop="about">(.+?(?=</span>))', regexpr("\n",data," "))
+        when repo='SourceForge' then regexpr('<meta property="og:description" content="(.+)"/>', data)
+        when repo='Google Code' then
+                case
+                        when regexprmatches('<meta name="hostname" content="github.com">', data) then regexpr('itemprop="about">(.+?(?=</span>))', regexpr("\n",data," "))
+                        else regexpr('<span itemprop="description">(.+)</span>', data)
+                end
+end as description,
+case
+        when repo='GitHub' then regexpr('<link rel="canonical" href="(.+)"', data)
+        when repo='SourceForge' then regexpr('<meta property="og:url" content="(.+)"/>', data)
+end as url
+from (select distinct id,match,repo, case when data is null then "" else data end as data from (
 select id,match,
 case
 when regexprmatches("https*\:\/\/github.com",match) then "GitHub"
-when regexprmatches("https*\:\/\/bitbucket.org\/",match) then "Bitbucket"                  
+when regexprmatches("https*\:\/\/bitbucket.org\/",match) then "Bitbucket"
 when regexprmatches("https*\:\/\/code\.google\.com",match) then "Google Code"
 when regexprmatches("https*\:\/\/sourceforge\.net",match) then "SourceForge"
-
-end as repo
-
-from 
-(select distinct id,regexpr("\W+$",match,"") as match
- from (select id,jfilterempty(jset(regexpr("((https*\:\/\/code\.google\.com\/p\/(?:\w+|\-))|(https*\:\/\/github.com\/.+\/(?:\w+|\-))|(https*\:\/\/bitbucket.org\/.+\/.+)|(https*\:\/\/sourceforge\.net\/projects\/.+))",middle))) as match from 
-    (setschema 'id,prev,middle,next' select c1 as id,textwindow2s(regexpr("\n",c2," "),0,1,0,"(https*\:\/\/code\.google\.com\/p\/(\w+|\-))|(https*\:\/\/github.com\/.+\/(\w+|\-))|(https*\:\/\/bitbucket.org\/.+\/.+)|(https*\:\/\/sourceforge\.net\/projects\/.+)") from 
-(select * from (setschema 'c1,c2' select jsonpath(c1, '$.id', '$.text') from stdinput()) where c2 is not null and c1 is not null)
-))));
+end as repo,
+urlrequest(null, unitosuni(match)) as data
+from
+(select id, regexpr("&gt",match,"") as match from (select distinct id,regexpr("\W+$",match,"") as match
+ from (select id,jfilterempty(jset(regexpr("(http.+)",middle))) as match from
+    (setschema 'id,prev,middle,next'
+select c1 as id,textwindow2s(regexpr("\n",c2," "),0,1,0,"(https*\:\/\/code\.google\.com\/p\/(\w+|\-))|(https*\:\/\/github.com\/.+\/(\w+|\-))|(https*\:\/\/bitbucket.org\/.+\/.+)|(https*\:\/\/sourceforge\.net\/projects\/.+)") from
+(setschema 'c1,c2' select * from pubs where c2 is not null and c1 is not null)
+))))))));
