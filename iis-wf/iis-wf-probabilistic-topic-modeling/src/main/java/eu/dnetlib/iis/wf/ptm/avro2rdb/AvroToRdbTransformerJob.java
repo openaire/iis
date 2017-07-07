b921d5d942a55e806ae292e8a9821aeeca2441e3
Marek Horst <mhorst@icm.edu.pl>
2017-07-07 10:38:33 +0200

    Closes #797: Integrate Probabilistic Topic Modeling with IIS
    Maintaining two rdb tables related to citations, taking citations matching outcome into account.
    Fixing other minor issues.
    Removing HDFS output support which was introduced for debugging purposes.
    Enabling rdb export of all data frames.

--- iis-wf/iis-wf-probabilistic-topic-modeling/src/main/java/eu/dnetlib/iis/wf/ptm/avro2rdb/AvroToRdbTransformerJob.java
+++ iis-wf/iis-wf-probabilistic-topic-modeling/src/main/java/eu/dnetlib/iis/wf/ptm/avro2rdb/AvroToRdbTransformerJob.java
@@ -16,9 +16,6 @@ import org.apache.spark.sql.execution.datasources.jdbc.JdbcUtils;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
-import com.google.common.base.Stopwatch;
-
-import eu.dnetlib.iis.common.java.io.HdfsUtils;
 
 
 public class AvroToRdbTransformerJob {
@@ -47,13 +44,6 @@ public class AvroToRdbTransformerJob {
         
         try (JavaSparkContext sc = new JavaSparkContext(conf)) {
             
-            HdfsUtils.remove(sc.hadoopConfiguration(), params.outputPublicationAvroPath);
-            HdfsUtils.remove(sc.hadoopConfiguration(), params.outputPubKeywordAvroPath);
-            HdfsUtils.remove(sc.hadoopConfiguration(), params.outputPubGrantAvroPath);
-            HdfsUtils.remove(sc.hadoopConfiguration(), params.outputCitationAvroPath);
-            HdfsUtils.remove(sc.hadoopConfiguration(), params.outputPubCitationAvroPath);
-            HdfsUtils.remove(sc.hadoopConfiguration(), params.outputReportPath);
-
             SQLContext sqlContext = new SQLContext(sc);
 
             // reading Avro data to dataframes
@@ -65,6 +55,8 @@ public class AvroToRdbTransformerJob {
             DataFrame project = sqlContext.read().format("com.databricks.spark.avro").load(params.inputProjectAvroPath);
             
             DataFrame documentToProject = sqlContext.read().format("com.databricks.spark.avro").load(params.inputDocumentToProjectAvroPath);
+            
+            DataFrame citation = sqlContext.read().format("com.databricks.spark.avro").load(params.inputCitationAvroPath);
             
             // ==============================================================================
             // Publication
@@ -91,13 +83,13 @@ public class AvroToRdbTransformerJob {
                     metadataFiltered.col("text").as("fulltext")
                     );
             
-            // writeToRdb(normalizedPublication, TABLE_PUBLICATION, params.postgresPassword);
+            writeToRdb(normalizedPublication, TABLE_PUBLICATION, params.postgresPassword);
             
             // ==============================================================================
             // Pub Grant
             // ==============================================================================
             
-            // FIXME make sure proper join type is used, inner join should be fine here
+            // make sure proper join type is used, inner join should be fine here
             // joinType One of: `inner`, `outer`, `left_outer`, `right_outer`, `leftsemi`
             DataFrame documentJoinedWithProjectDetails = documentToProject.join(
                     project, documentToProject.col("projectId").equalTo(project.col("id")));
@@ -110,7 +102,7 @@ public class AvroToRdbTransformerJob {
                             documentJoinedWithProjectDetails.col("fundingClass").as("funder")
                     );
             
-            // writeToRdb(normalizedPubGrant, TABLE_PUB_GRANT, params.postgresPassword);
+            writeToRdb(normalizedPubGrant, TABLE_PUB_GRANT, params.postgresPassword);
             
             // ==============================================================================
             // PubKeyword
@@ -120,29 +112,30 @@ public class AvroToRdbTransformerJob {
                     metadata.col("id").as("pubId"),
                     explode(metadata.col("keywords")).as("keyword"));
             
-            // writeToRdb(metadataKeywordsExploded, TABLE_PUB_KEYWORD, params.postgresPassword);
+            writeToRdb(metadataKeywordsExploded, TABLE_PUB_KEYWORD, params.postgresPassword);
             
             // ==============================================================================
             // Citation
             // ==============================================================================
-            // TODO in fact we are interested only in matched citations, so we should rely on citationmatching outcome rather than references
+            // TODO we should handle externally matched citations as well
+            DataFrame citationInternal = citation.filter("entry.destinationDocumentId is not null").select(
+                    citation.col("sourceDocumentId").as("pubId"),
+                    citation.col("entry.destinationDocumentId").as("citationId"),
+                    citation.col("entry.rawText").as("reference"));
             
-            DataFrame metadataReferencesExploded = metadata.select(
-                    metadata.col("id").as("pubId"),
-                    explode(metadata.col("references.text")).as("reference"));
-            // metadataReferencesExploded.printSchema();
-            // notice: currently we cannot write to this table due PK restriction, we do have duplicates
-            // writeToRdb(metadataReferencesExploded, TABLE_CITATION);
-            
-            // https://stackoverflow.com/questions/30501300/is-spark-dataframe-nested-structure-limited-for-selection
-            // there is a problem with arrays and lists from java beans
-            // https://stackoverflow.com/questions/35986157/creating-a-dataframe-out-of-nested-user-defined-objects
+            DataFrame citationInternalDedupedByCitationIdWithReferences = citationInternal.select(
+                    citationInternal.col("citationId"),
+                    citationInternal.col("reference")).dropDuplicates(new String[] {"citationId"});
+            // dropping duplicates by citationId is required due to PK restriction
+            writeToRdb(citationInternalDedupedByCitationIdWithReferences, TABLE_CITATION, params.postgresPassword);
             
             // ==============================================================================
             // Pub Citation
             // ==============================================================================
-
-            
+            DataFrame citationInternalDedupedWithoutReference = citationInternal.select(
+                    citationInternal.col("pubId"),
+                    citationInternal.col("citationId")).dropDuplicates();
+            writeToRdb(citationInternalDedupedWithoutReference, TABLE_PUB_CITATION, params.postgresPassword);
             // ==============================================================================
             
         }
@@ -193,26 +186,11 @@ public class AvroToRdbTransformerJob {
         @Parameter(names = "-inputDocumentToProjectAvroPath", required = true)
         private String inputDocumentToProjectAvroPath;
 
+        @Parameter(names = "-inputCitationAvroPath", required = true)
+        private String inputCitationAvroPath;
         
         @Parameter(names = "-confidenceLevelThreshold", required = true)
         private Float confidenceLevelThreshold;
-        
-       
-        @Parameter(names = "-outputPublicationAvroPath", required = true)
-        private String outputPublicationAvroPath;
-        
-        @Parameter(names = "-outputPubKeywordAvroPath", required = true)
-        private String outputPubKeywordAvroPath;
-        
-        @Parameter(names = "-outputPubGrantAvroPath", required = true)
-        private String outputPubGrantAvroPath;
-        
-        @Parameter(names = "-outputCitationAvroPath", required = true)
-        private String outputCitationAvroPath;
-        
-        @Parameter(names = "-outputPubCitationAvroPath", required = true)
-        private String outputPubCitationAvroPath;
-        
         
         @Parameter(names = "-outputReportPath", required = true)
         private String outputReportPath;
