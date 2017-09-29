66330a0c25aed09d73344c18d68c848b01546533
Marek Horst <mhorst@icm.edu.pl>
2017-09-29 16:11:10 +0200

    Closes #817: Integrate community reference extraction algorithm
    Phase 1: generating inference output for validation purposes. Introducing DocumentToCommunity schema and wrapping madis script with oozie action.

---
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/community/main/oozie_app/lib/scripts/oac_comm_IIS_run.sql
@@ -0,0 +1,34 @@
+PRAGMA temp_store_directory = '.';
+
+create temp table pubs as setschema 'c1,c2' select jsonpath(c1, '$.id', '$.text') from stdinput();
+
+select jdict('documentId', docid, 'projectId', 'CLARIN', 'confidenceLevel', 1, 'match', middle, 'context', context) as C1 from (
+select docid, stripchars(middle,'.)(,[]') as middle, prev||" "||middle||" "||next as context
+from (
+  setschema 'docid,prev,middle,next' select c1, textwindow2s(comprspaces(regexpr("\n", C2, " ")),20,1,10, '\bCLARIN\b') from pubs where c2 is not null
+)) group by docid
+
+union all
+
+select jdict('documentId', docid, 'projectId', 'ANR', 'confidenceLevel', 1, 'match', middle, 'context', context) as C1 from (
+select docid, stripchars(middle,'.)(,[]') as middle, prev||" "||middle||" "||next as context
+from (
+  setschema 'docid,prev,middle,next' select c1, textwindow2s(comprspaces(regexpr("\n", C2, " ")),20,3,10, '(?:\bANR-\d{2}-\w{4}-\d{4}\b)|(?:\bFrance Life Imaging\b)') from pubs where c2 is not null
+)) group by docid
+
+union all
+
+select jdict('documentId', docid, 'projectId', 'FLI-IAM', 'confidenceLevel', 1, 'match', middle, 'context', context) as C1 from (
+select docid, stripchars(middle,'.)(,[]') as middle, prev||" "||middle||" "||next as context
+from (
+  setschema 'docid,prev,middle,next' select c1, textwindow2s(comprspaces(regexpr("\n", C2, " ")),20,4,10, '(?:\bFrance Life Imaging\b)|(?:\bFLI-IAM\b)|(?:\bInformation Analysis and Management\b)') from pubs where c2 is not null
+)) group by docid
+
+union all
+
+select jdict('documentId', docid, 'projectId', 'SDSN', 'confidenceLevel', 1, 'match', middle, 'context', context) as C1 from (
+select docid, stripchars(middle,'.)(,[]') as middle, prev||" "||middle||" "||next as context
+from (
+  setschema 'docid,prev,middle,next' select c1, textwindow2s(comprspaces(regexpr("\n", C2, " ")),20,2,10, '(?:\bSDSN\s)|(?:\bSDSN Greece\b)') from pubs where c2 is not null
+)) group by docid
+;
