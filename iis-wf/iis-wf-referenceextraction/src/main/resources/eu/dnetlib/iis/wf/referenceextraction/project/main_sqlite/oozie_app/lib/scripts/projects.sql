70046f81a85b9671b90dae5feb5010f40fca43cf
TasosGi <sospioneer2002@gmail.com>
2017-10-31 11:57:07 +0200

    Implementation for Academy of Finland

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/project/main_sqlite/oozie_app/lib/scripts/projects.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/project/main_sqlite/oozie_app/lib/scripts/projects.sql
@@ -24,6 +24,20 @@ regexpcountwords('(?:\bwell?come trust\b)|(?:(?:\bthis work was|financial(?:ly)?
 
 create temp table output_table as 
 
+select jdict('documentId', docid, 'projectId', id, 'confidenceLevel', sqroot(min(1.49,confidence)/1.5)) as C1, docid, id, fundingclass1 from ( select docid,id,confidence, docid, id,  fundingclass1 from ( select 
+0.8 as confidence, docid, id, fundingclass1
+from (
+unindexed select docid, regexpr("(\d+)",middle) as middle, comprspaces(j2s(prev1,prev2,prev3,prev4,prev5,prev6,prev7,prev8,prev9,prev10,prev11,prev12,prev13,middle,next)) as context
+from (
+  setschema 'docid,prev1,prev2,prev3,prev4,prev5,prev6,prev7,prev8,prev9,prev10,prev11,prev12,prev13,middle,next' select c1, textwindow(lower(c2),-13,0,1, '\b\d{5,6}\b') from pubs where c2 is not null
+) where CAST(regexpr("(\d+)",middle) AS INT)>70000), grants
+WHERE fundingclass1="AFF" and (regexprmatches("[\b\s]academy of finland[\b\s]", context) or regexprmatches("[\b\s]finnish (?:(?:programme for )?cent(?:re|er)s? of excellence|national research council|funding agency|research program)[\b\s]", context) or regexprmatches("[\b\s]finnish academy[\b\s]", context)) and grantid=middle
+) group by docid,id)
+
+
+union all
+
+
 select jdict('documentId', docid, 'projectId', id, 'confidenceLevel', sqroot(min(1.49,confidence)/1.5)) as C1, docid, id, fundingclass1 from ( select docid,id,max(confidence) as confidence, docid, id,  fundingclass1 from ( select 
 case when keywordmatch then 0.9 else generalmatch*0.25 end as confidence, docid, 
 case when keywordmatch then id else var('tara_unidentified') end as id, fundingclass1
