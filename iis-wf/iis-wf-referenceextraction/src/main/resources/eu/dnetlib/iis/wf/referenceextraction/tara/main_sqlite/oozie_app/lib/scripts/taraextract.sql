2b03c2af228b8ee9aa90ce8e3f2504c2567e35e1
TasosGi <sospioneer2002@gmail.com>
2018-03-09 23:54:22 +0200

    Fixed: no such column: prev

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/tara/main_sqlite/oozie_app/lib/scripts/taraextract.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/tara/main_sqlite/oozie_app/lib/scripts/taraextract.sql
@@ -16,7 +16,7 @@ delete from pubs where regexprmatches("\btara\b", lower(c2)) = 0;
 insert into tara_match 
 select c1 as documentid, id as projectid, 0.8 as confidence from (
 select c1, id, max(matches) as count from 
-( select c1, id, regexpcountwords('\b'||tarakeywords||'\b', c2) as matches from (
+( setschema 'c1,id,matches' select c1, id, regexpcountwords('\b'||tarakeywords||'\b', c2) as matches from (
 setschema 'c1,c2' select c1, lower(comprspaces(regexpr('\n',c2,' '))) from pubs where c2 is not null and c1 not in (select distinct documentid from tara_match)), grants where tarakeywords!="" and matches > 10
 ) group by c1
 );
@@ -24,7 +24,7 @@ setschema 'c1,c2' select c1, lower(comprspaces(regexpr('\n',c2,' '))) from pubs 
 insert into tara_match 
 select c1 as  documentid, case when count>0 then id else case when var('tara_unidentified') and generalmatch > 0 then var('tara_unidentified') else null end end as projectid, 0.7 as confidence from (
 select c1, id, max(matches) as count, regexpcountwords('(?:(?:\bfou?ndation)? tara expeditions?\b)|(?:\btara[ -]{1,2}(?:arctic|oceans?|pacific|med|girus|funding)\b)|(?:\btara transpolar drift\b)', prev||" "||middle||" "||next) as generalmatch from 
-( select c1, id, regexpcountwords('\b'||tarakeywords||'\b', c2) as matches, textwindow2s(keywords(c2), 4,1,4, '(\b(?:tara)\b)') from (
+( setschema 'c1,id,matches,prev,middle,next' select c1, id, regexpcountwords('\b'||tarakeywords||'\b', c2) as matches, textwindow2s(keywords(c2), 4,1,4, '(\b(?:tara)\b)') from (
 setschema 'c1,c2' select c1, textacknowledgmentstara(lower(comprspaces(regexpr('\n',c2,' ')))) from pubs where c2 is not null and c1 not in (select distinct documentid from tara_match)), grants where tarakeywords!=""
 ) group by c1
 ) where projectid not null;
