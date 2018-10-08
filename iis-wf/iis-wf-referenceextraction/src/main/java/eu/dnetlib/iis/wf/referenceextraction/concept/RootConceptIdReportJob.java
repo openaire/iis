846bee5632ece39b1a0ed181fb496883d4d7f381
Marek Horst <mhorst@icm.edu.pl>
2018-10-08 11:10:08 +0200

    Closes #916: Introduce detailed counters for community and project references grouped by main concept id
    Renaming existing counters:
    * `processing.referenceExtraction.community.reference` -> `processing.referenceExtraction.community.reference.total`
    * `processing.referenceExtraction.concept.reference` -> `processing.referenceExtraction.concept.reference.total`
    
    Introducing new counters grouped by root context identifier generated as:
    * `processing.referenceExtraction.community.reference.byrootid.{rootContextId}`
    * `processing.referenceExtraction.concept.reference.byrootid.{rootContextId}`

---
+++ iis-wf/iis-wf-referenceextraction/src/main/java/eu/dnetlib/iis/wf/referenceextraction/concept/RootConceptIdReportJob.java
@@ -0,0 +1,114 @@
+package eu.dnetlib.iis.wf.referenceextraction.concept;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+import eu.dnetlib.iis.common.java.io.HdfsUtils;
+import eu.dnetlib.iis.common.schemas.ReportEntry;
+import eu.dnetlib.iis.common.schemas.ReportEntryType;
+import eu.dnetlib.iis.referenceextraction.researchinitiative.schemas.DocumentToConceptId;
+import pl.edu.icm.sparkutils.avro.SparkAvroLoader;
+import pl.edu.icm.sparkutils.avro.SparkAvroSaver;
+import scala.Tuple2;
+
+
+/**
+ * Generates {@link DocumentToConceptId} relation counters grouped by root concept identifiers.
+ * 
+ * @author mhorst
+ *
+ */
+public class RootConceptIdReportJob {
+    
+    private static final String CONCEPT_SEPARATOR = "::";
+    
+    private static final String ROOT_CONCEPT_ID_TOKEN = "#{rootConceptId}";
+    
+    private static SparkAvroLoader avroLoader = new SparkAvroLoader();
+    private static SparkAvroSaver avroSaver = new SparkAvroSaver();
+    
+    
+    //------------------------ LOGIC --------------------------
+    
+    public static void main(String[] args) throws IOException {
+        
+        RootConceptIdReportJobParameters params = new RootConceptIdReportJobParameters();
+        JCommander jcommander = new JCommander(params);
+        jcommander.parse(args);
+        
+        SparkConf conf = new SparkConf();
+        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+        conf.set("spark.kryo.registrator", "pl.edu.icm.sparkutils.avro.AvroCompatibleKryoRegistrator");
+        
+        try (JavaSparkContext sc = new JavaSparkContext(conf)) {
+            
+            HdfsUtils.remove(sc.hadoopConfiguration(), params.outputReportPath);
+            
+            JavaRDD<DocumentToConceptId> documentToConcept = avroLoader.loadJavaRDD(sc, params.inputDocumentToConceptAvroPath, DocumentToConceptId.class);
+
+            JavaPairRDD<CharSequence, Integer> rootConceptIdToOne = documentToConcept.mapToPair(x -> new Tuple2<CharSequence, Integer>(extractRootConceptId(x.getConceptId()), 1));
+            JavaPairRDD<CharSequence, Integer> reducedRootConceptIdWithCount = rootConceptIdToOne.reduceByKey((x, y) -> x+y);
+
+            JavaRDD<ReportEntry> conceptReport = convertToReportEntries(reducedRootConceptIdWithCount.sortByKey(true), params.reportKeyTemplate);
+            
+            avroSaver.saveJavaRDD(conceptReport, ReportEntry.SCHEMA$, params.outputReportPath);
+        }
+        
+    }
+    
+    //------------------------ PRIVATE --------------------------
+
+    /**
+     * Extracts root contcept identifier from specific conceptId.
+     * Never returns null, "unknown" value is returned when funder detail is not available. 
+     */
+    static String extractRootConceptId(CharSequence conceptId) {
+        if (StringUtils.isNotBlank(conceptId)) {
+            String conceptIdStr = conceptId.toString();
+            if (conceptIdStr.contains(CONCEPT_SEPARATOR)) {
+                if (!conceptIdStr.startsWith(CONCEPT_SEPARATOR)) {
+                    String rootConceptId = StringUtils.splitByWholeSeparator(conceptIdStr, CONCEPT_SEPARATOR)[0];
+                    if (!StringUtils.isBlank(rootConceptId)) {
+                        return rootConceptId;
+                    }    
+                }
+            } else {
+                return conceptIdStr;
+            }
+        }
+        return "unknown";
+    }
+    
+    /**
+     * Converts all funder names into report entry keys using template and replacing FUNDER_TOKEN with real funder name.
+     */
+    static JavaRDD<ReportEntry> convertToReportEntries(JavaPairRDD<CharSequence, Integer> source,
+            String reportKeyTemplate) {
+        return source.map(x -> new ReportEntry(
+                StringUtils.replace(reportKeyTemplate, ROOT_CONCEPT_ID_TOKEN, x._1.toString().toLowerCase()), 
+                ReportEntryType.COUNTER, String.valueOf(x._2)));
+    }
+    
+    @Parameters(separators = "=")
+    private static class RootConceptIdReportJobParameters {
+
+        @Parameter(names = "-inputDocumentToConceptAvroPath", required = true)
+        private String inputDocumentToConceptAvroPath;
+        
+        @Parameter(names = "-outputReportPath", required = true)
+        private String outputReportPath;
+        
+        @Parameter(names = "-reportKeyTemplate", required = true)
+        private String reportKeyTemplate;
+
+    }
+}
---
+++ iis-wf/iis-wf-referenceextraction/src/test/java/eu/dnetlib/iis/wf/referenceextraction/concept/RootConceptIdReportJobTest.java
@@ -0,0 +1,91 @@
+package eu.dnetlib.iis.wf.referenceextraction.concept;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.io.Files;
+
+import eu.dnetlib.iis.common.schemas.ReportEntry;
+import eu.dnetlib.iis.common.utils.AvroAssertTestUtil;
+import eu.dnetlib.iis.common.utils.AvroTestUtils;
+import eu.dnetlib.iis.common.utils.JsonAvroTestUtils;
+import eu.dnetlib.iis.referenceextraction.researchinitiative.schemas.DocumentToConceptId;
+import pl.edu.icm.sparkutils.test.SparkJob;
+import pl.edu.icm.sparkutils.test.SparkJobBuilder;
+import pl.edu.icm.sparkutils.test.SparkJobExecutor;
+
+/**
+ * 
+ * @author mhorst
+ *
+ */
+public class RootConceptIdReportJobTest {
+
+    private SparkJobExecutor executor = new SparkJobExecutor();
+    
+    private File workingDir;
+    
+    private String inputDocumentToConceptDirPath;
+    
+    private String outputReportDirPath;
+    
+    
+    @Before
+    public void before() {
+        workingDir = Files.createTempDir();
+        inputDocumentToConceptDirPath = workingDir + "/root_concept_report/input_document_to_concept";
+        outputReportDirPath = workingDir + "/root_concept_report/output_report";
+    }
+    
+    
+    @After
+    public void after() throws IOException {
+        FileUtils.deleteDirectory(workingDir);
+    }
+    
+    
+    //------------------------ TESTS --------------------------
+    
+    @Test
+    public void generateReport() throws IOException {
+        
+        // given
+        String jsonInputDocumentToConceptFile = "src/test/resources/eu/dnetlib/iis/wf/referenceextraction/concept/root_conceptid_report/data/input_document_to_concept.json";
+        String jsonOutputReportFile = "src/test/resources/eu/dnetlib/iis/wf/referenceextraction/concept/root_conceptid_report/data/output_report.json";
+
+        AvroTestUtils.createLocalAvroDataStore(
+                JsonAvroTestUtils.readJsonDataStore(jsonInputDocumentToConceptFile, DocumentToConceptId.class),
+                inputDocumentToConceptDirPath);
+        
+        // execute
+        executor.execute(buildRootConceptIdReportJob(inputDocumentToConceptDirPath, outputReportDirPath));
+        
+        // assert
+        AvroAssertTestUtil.assertEqualsWithJsonIgnoreOrder(outputReportDirPath, jsonOutputReportFile, ReportEntry.class);
+    }
+    
+    //------------------------ PRIVATE --------------------------
+    
+    
+    private SparkJob buildRootConceptIdReportJob(String inputDocumentToConceptDirPath, String outputReportDirPath) {
+        SparkJob sparkJob = SparkJobBuilder
+                .create()
+                
+                .setAppName("Spark Report Generator")
+
+                .setMainClass(RootConceptIdReportJob.class)
+                .addArg("-inputDocumentToConceptAvroPath", inputDocumentToConceptDirPath)
+                .addArg("-outputReportPath", outputReportDirPath)
+                .addArg("-reportKeyTemplate", "processing.referenceExtraction.concept.reference.byrootid.#{rootConceptId}")
+                .addJobProperty("spark.driver.host", "localhost")
+                
+                .build();
+        
+        return sparkJob;
+    }
+}
