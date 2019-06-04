b77fb8b041dcaeb69586825d0fff3fad293e4d26
Marek Horst <mhorst@icm.edu.pl>
2019-06-04 14:44:08 +0200

    Closes #951: Switch madis to rely on pypy instead of python command
    Temporarily reverting pypy support in dataset and community reference extraction modules due to job failures.

--- iis-wf/iis-wf-referenceextraction/src/main/java/eu/dnetlib/iis/wf/referenceextraction/dataset/DatasetDBBuilder.java
+++ iis-wf/iis-wf-referenceextraction/src/main/java/eu/dnetlib/iis/wf/referenceextraction/dataset/DatasetDBBuilder.java
@@ -39,7 +39,7 @@ public class DatasetDBBuilder extends AbstractDBBuilder<DataSetReference> {
         targetDbFile.setWritable(true);
 
         return new ProcessExecutionContext(
-                Runtime.getRuntime().exec("/usr/local/sbin/pypy scripts/madis/mexec.py -w " + targetDbLocation + " -f " + scriptLocation),
+                Runtime.getRuntime().exec("python scripts/madis/mexec.py -w " + targetDbLocation + " -f " + scriptLocation),
                 targetDbFile);
     }
 
