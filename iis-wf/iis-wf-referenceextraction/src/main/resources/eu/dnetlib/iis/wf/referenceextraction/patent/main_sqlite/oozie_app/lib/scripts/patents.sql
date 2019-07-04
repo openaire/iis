a72910ed2c98c6cabbf6f9ad74c781399c2c1803
johnfouf <johnfouf@gmail.com>
2019-07-04 13:14:42 +0300

    update patent rules

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
@@ -7,8 +7,9 @@ select  docid, appln_id as id, prev||" "||middle||" "||next as context from
 (setschema 'docid,prev,middle,next' select c1 as docid,textwindow2s(keywords(c2),10,1,4, "(?:\D|\b)\d{6,12}\b") from (setschema 'c1,c2' select * from pubs)), patents
 where regexpr("(?:\D|\b)(\d{6,12})\b",middle) = appln_nr 
 and (regexprmatches("european patent|patent application|patent office|ep patent|eu patent|patent",lower(context)) or 
-    regexprmatches("\bEP(?:\d|\b)|\bEPO(?:\d|\b)",context)   
+    regexprmatches("\bEPO(?:\d|\b)",context) or regexprmatches ("EP\s*"||appln_nr,context)
     )
+and (not regexprmatches("patent ductus|patent foramen|arxiv|cern|biol|clin|letters|report",lower(context)))
 
 
 );
