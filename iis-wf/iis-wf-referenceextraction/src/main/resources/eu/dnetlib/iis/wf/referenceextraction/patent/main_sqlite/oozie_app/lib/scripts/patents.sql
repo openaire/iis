7d7ea993280cd630f905bd67ecab3fa191795364
johnfouf <johnfouf@gmail.com>
2019-07-18 18:02:20 +0300

    update patent rules v2

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
@@ -3,13 +3,13 @@ create temp table pubs as setschema 'c1,c2' select jsonpath(c1, '$.id', '$.text'
 
 select jdict('documentId', docid, 'patentId', id, 'confidenceLevel', 0.8,'textsnippet',context) from (
 
-select  docid, appln_id as id, prev||" "||middle||" "||next as context from 
+select  docid, appln_id as id, prev||" "||middle||" "||next as context, appln_nr from 
 (setschema 'docid,prev,middle,next' select c1 as docid,textwindow2s(keywords(c2),10,1,4, "(?:\D|\b)\d{6,12}\b") from (setschema 'c1,c2' select * from pubs)), patents
 where regexpr("(?:\D|\b)(\d{6,12})\b",middle) = appln_nr 
 and (regexprmatches("european patent|patent application|patent office|ep patent|eu patent|patent",lower(context)) or 
-    regexprmatches("\bEPO(?:\d|\b)",context) or regexprmatches ("EP\s*"||appln_nr,context)
-    )
-and (not regexprmatches("patent ductus|patent foramen|arxiv|cern|biol|clin|letters|report",lower(context)))
+    regexprmatches("\bEPO(?:\d|\b)",context) or regexprmatches ("\bEP\s*"||regexpr("(?:\D|\b)(\d{6,12})\b",middle),context)
+    ) 
+and (not regexprmatches("holotype|paratype|specimen|dissection|\bnih\b|hepth|barcode|\bstrain|accession|\bbacter\b|patent ductus|patent foramen|arxiv|cern|biol|clin|letters|report",lower(context)))
 
 
 );
