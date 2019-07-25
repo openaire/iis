9743f350e156aadda1f088beb95eb1c382239406
johnfouf <johnfouf@gmail.com>
2019-07-25 15:28:52 +0300

    solve patent mining error

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
@@ -7,7 +7,7 @@ select  docid, appln_id as id, prev||" "||middle||" "||next as context, appln_nr
 (setschema 'docid,prev,middle,next' select c1 as docid,textwindow2s(keywords(c2),10,1,4, "(?:\D|\b)\d{6,12}\b") from (setschema 'c1,c2' select * from pubs)), patents
 where regexpr("(?:\D|\b)(\d{6,12})\b",middle) = appln_nr 
 and (regexprmatches("european patent|patent application|patent office|ep patent|eu patent|patent",lower(context)) or 
-    regexprmatches("\bEPO(?:\d|\b)",context) or regexprmatches ("\bEP\s*"||regexpr("(?:\D|\b)(\d{6,12})\b",middle),context)
+    regexprmatches("\bEPO(?:\d|\b)",context) or regexprmatches ("\bEP\s*"||appln_nr,context)
     ) 
 and (not regexprmatches("holotype|paratype|specimen|dissection|\bnih\b|hepth|barcode|\bstrain|accession|\bbacter\b|patent ductus|patent foramen|arxiv|cern|biol|clin|letters|report",lower(context)))
 
