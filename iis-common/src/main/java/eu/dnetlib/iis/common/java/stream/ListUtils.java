e38b5a87799ff151dd724e3b0721c2dc67fd8b29
pjacewicz <przemyslaw.jacewicz@icm.edu.pl>
2019-10-17 15:45:04 +0200

    Add list zipping

---
+++ iis-common/src/main/java/eu/dnetlib/iis/common/java/stream/ListUtils.java
@@ -0,0 +1,21 @@
+package eu.dnetlib.iis.common.java.stream;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ListUtils {
+
+    public static <X, Y> List<Pair<X, Y>> zip(List<X> left, List<Y> right) {
+        if (left.size() != right.size()) {
+            throw new IllegalArgumentException(String.format("List sizes do not match: left=%d, right=%d",
+                    left.size(), right.size()));
+        }
+        return IntStream.range(0, left.size())
+                .mapToObj(i -> Pair.of(left.get(i), right.get(i)))
+                .collect(Collectors.toList());
+    }
+
+}
