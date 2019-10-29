da078c41029fa5eea7bd1e762b451b66afba9689
johnfouf <johnfouf@gmail.com>
2019-10-29 12:35:22 +0200

    update patent mining - add authors v2

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
@@ -11,7 +11,7 @@ stdinput() );
 
 create temp table results as select * from (
 select  docid, authors, appln_id as id, prev||" "||middle||" "||next as context, appln_nr from 
-(setschema 'docid,prev,middle,next' select c1 as docid,c3 as authors, textwindow2s(keywords(c2),15,1,7, "(?:\D|\b)\d{6,12}\b") from (setschema 'c1,c2,c3' select * from pubs)), patents
+(setschema 'docid,authors,prev,middle,next' select c1 as docid,c3 as authors, textwindow2s(keywords(c2),25,1,10, "(?:\D|\b)\d{6,12}\b") from (setschema 'c1,c2,c3' select * from pubs)), patents
 where regexpr("(?:\D|\b)(\d{6,12})\b",middle) = appln_nr 
 and (regexprmatches("european patent|patent application|patent office|ep patent|eu patent|patent",lower(context)) or 
     regexprmatches("\bEPO(?:\d|\b)",context) or regexprmatches ("\bEP\s*"||appln_nr,context)
@@ -22,6 +22,6 @@ and (not regexprmatches("[0-9]\."||appln_nr,middle) )
 );
 
 
-select jdict('documentId', docid, 'patentId', id, 'confidenceLevel', 0.8,'textsnippet',context)  from results where regexprmatches(authors,context);
+select jdict('documentId', docid, 'patentId', id, 'confidenceLevel', 0.8,'textsnippet',context)  from results; -- where regexprmatches(authors,context);
 
 
