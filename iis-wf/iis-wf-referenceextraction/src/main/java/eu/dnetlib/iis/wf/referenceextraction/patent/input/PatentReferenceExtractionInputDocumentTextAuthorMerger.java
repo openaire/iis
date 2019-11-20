9b719183fb7c8b1718976252529969f143adad05
pjacewicz <przemyslaw.jacewicz@icm.edu.pl>
2019-11-21 00:22:16 +0100

    iis-wf-referenceextraction: document authors added to patent mining

---
+++ iis-wf/iis-wf-referenceextraction/src/main/java/eu/dnetlib/iis/wf/referenceextraction/patent/input/PatentReferenceExtractionInputDocumentTextAuthorMerger.java
@@ -0,0 +1,82 @@
+package eu.dnetlib.iis.wf.referenceextraction.patent.input;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import eu.dnetlib.iis.common.java.io.HdfsUtils;
+import eu.dnetlib.iis.importer.schemas.Author;
+import eu.dnetlib.iis.metadataextraction.schemas.DocumentText;
+import eu.dnetlib.iis.referenceextraction.patent.schemas.DocumentTextWithAuthors;
+import eu.dnetlib.iis.transformers.metadatamerger.schemas.ExtractedDocumentMetadataMergedWithOriginal;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import pl.edu.icm.sparkutils.avro.SparkAvroLoader;
+import pl.edu.icm.sparkutils.avro.SparkAvroSaver;
+import scala.Tuple2;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class PatentReferenceExtractionInputDocumentTextAuthorMerger {
+    private static final SparkAvroLoader sparkAvroLoader = new SparkAvroLoader();
+    private static final SparkAvroSaver sparkAvroSaver = new SparkAvroSaver();
+
+    public static void main(String[] args) throws IOException {
+        JobParameters params = new JobParameters();
+        JCommander jcommander = new JCommander(params);
+        jcommander.parse(args);
+
+        SparkConf conf = new SparkConf();
+        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+        conf.set("spark.kryo.registrator", "pl.edu.icm.sparkutils.avro.AvroCompatibleKryoRegistrator");
+
+        try (JavaSparkContext sc = new JavaSparkContext(conf)) {
+            HdfsUtils.remove(sc.hadoopConfiguration(), params.outputPath);
+
+            JavaRDD<DocumentText> documentTexts = sparkAvroLoader.loadJavaRDD(sc, params.inputDocumentTextPath, DocumentText.class);
+            JavaRDD<ExtractedDocumentMetadataMergedWithOriginal> documentMetadatas =
+                    sparkAvroLoader.loadJavaRDD(sc, params.inputExtractedDocumentMetadataMergedWithOriginalPath, ExtractedDocumentMetadataMergedWithOriginal.class);
+
+            JavaPairRDD<CharSequence, DocumentText> documentTextsById = documentTexts
+                    .mapToPair(x -> new Tuple2<>(x.getId(), x));
+
+            JavaPairRDD<CharSequence, List<CharSequence>> documentMetadatasById = documentMetadatas
+                    .filter(x -> Objects.nonNull(x.getImportedAuthors()))
+                    .filter(x -> x.getImportedAuthors().stream().allMatch(y -> StringUtils.isNotBlank(y.getFullname())))
+                    .mapToPair(x -> new Tuple2<>(x.getId(), x.getImportedAuthors().stream().map(Author::getFullname).collect(Collectors.toList())));
+
+            JavaRDD<DocumentTextWithAuthors> merged = documentTextsById
+                    .join(documentMetadatasById)
+                    .values()
+                    .map(PatentReferenceExtractionInputDocumentTextAuthorMerger::merge);
+            sparkAvroSaver.saveJavaRDD(merged, DocumentTextWithAuthors.SCHEMA$, params.outputPath);
+        }
+    }
+
+    private static DocumentTextWithAuthors merge(Tuple2<DocumentText, List<CharSequence>> pair) {
+        DocumentText documentText = pair._1();
+        List<CharSequence> authors = pair._2();
+        return DocumentTextWithAuthors.newBuilder()
+                .setId(documentText.getId())
+                .setText(documentText.getText())
+                .setAuthors(authors)
+                .build();
+    }
+
+    @Parameters(separators = "=")
+    private static class JobParameters {
+        @Parameter(names = "-inputDocumentTextPath", required = true)
+        private String inputDocumentTextPath;
+
+        @Parameter(names = "-inputExtractedDocumentMetadataMergedWithOriginalPath", required = true)
+        private String inputExtractedDocumentMetadataMergedWithOriginalPath;
+
+        @Parameter(names = "-outputPath", required = true)
+        private String outputPath;
+    }
+}
