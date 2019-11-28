11799183dbf47a0a546d3be688c198f928f4ba99
pjacewicz <przemyslaw.jacewicz@icm.edu.pl>
2019-11-28 12:38:23 +0100

    iis:wf-referenceextraction: added missing output dire removal plus minor fixes

--- iis-wf/iis-wf-referenceextraction/src/main/java/eu/dnetlib/iis/wf/referenceextraction/patent/input/PatentReferenceExtractionInputTransformerJob.java
+++ iis-wf/iis-wf-referenceextraction/src/main/java/eu/dnetlib/iis/wf/referenceextraction/patent/input/PatentReferenceExtractionInputTransformerJob.java
@@ -3,6 +3,7 @@ package eu.dnetlib.iis.wf.referenceextraction.patent.input;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import eu.dnetlib.iis.common.java.io.HdfsUtils;
 import eu.dnetlib.iis.referenceextraction.patent.schemas.Patent;
 import eu.dnetlib.iis.referenceextraction.patent.schemas.PatentReferenceExtractionInput;
 import org.apache.spark.SparkConf;
@@ -11,13 +12,15 @@ import org.apache.spark.api.java.JavaSparkContext;
 import pl.edu.icm.sparkutils.avro.SparkAvroLoader;
 import pl.edu.icm.sparkutils.avro.SparkAvroSaver;
 
+import java.io.IOException;
+
 public class PatentReferenceExtractionInputTransformerJob {
     private static final SparkAvroLoader sparkAvroLoader = new SparkAvroLoader();
     private static final SparkAvroSaver sparkAvroSaver = new SparkAvroSaver();
 
     //------------------------ LOGIC --------------------------
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         JobParameters params = new JobParameters();
         JCommander jcommander = new JCommander(params);
         jcommander.parse(args);
@@ -27,6 +30,8 @@ public class PatentReferenceExtractionInputTransformerJob {
         conf.set("spark.kryo.registrator", "pl.edu.icm.sparkutils.avro.AvroCompatibleKryoRegistrator");
 
         try (JavaSparkContext sc = new JavaSparkContext(conf)) {
+            HdfsUtils.remove(sc.hadoopConfiguration(), params.outputPath);
+
             JavaRDD<PatentReferenceExtractionInput> convertedRDD = sparkAvroLoader.loadJavaRDD(sc, params.inputPath, Patent.class)
                     .map(PatentReferenceExtractionInputTransformerJob::convert);
             sparkAvroSaver.saveJavaRDD(convertedRDD, PatentReferenceExtractionInput.SCHEMA$, params.outputPath);
