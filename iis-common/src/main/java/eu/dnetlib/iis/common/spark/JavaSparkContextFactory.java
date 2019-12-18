74c8f9c77edf8ae7ccd76f61bf1cf8f6ea30e27d
pjacewicz <przemyslaw.jacewicz@icm.edu.pl>
2019-12-18 18:27:46 +0100

    [iis-common] spark conf helper introduced

--- iis-common/src/main/java/eu/dnetlib/iis/common/spark/JavaSparkContextFactory.java
+++ iis-common/src/main/java/eu/dnetlib/iis/common/spark/JavaSparkContextFactory.java
@@ -7,13 +7,13 @@ import org.apache.spark.sql.SparkSession;
 public class JavaSparkContextFactory {
 
     private JavaSparkContextFactory() {
-
     }
 
     public static JavaSparkContext withConfAndKryo(SparkConf conf) {
-        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-        conf.set("spark.kryo.registrator", "pl.edu.icm.sparkutils.avro.AvroCompatibleKryoRegistrator");
         return JavaSparkContext
-                .fromSparkContext(SparkSession.builder().config(conf).getOrCreate().sparkContext());
+                .fromSparkContext(SparkSession.builder()
+                        .config(SparkConfHelper.withKryo(conf))
+                        .getOrCreate()
+                        .sparkContext());
     }
 }
--- iis-common/src/main/java/eu/dnetlib/iis/common/spark/SparkSessionFactory.java
+++ iis-common/src/main/java/eu/dnetlib/iis/common/spark/SparkSessionFactory.java
@@ -9,10 +9,8 @@ public class SparkSessionFactory {
     }
 
     public static SparkSession withConfAndKryo(SparkConf conf) {
-        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-        conf.set("spark.kryo.registrator", "pl.edu.icm.sparkutils.avro.AvroCompatibleKryoRegistrator");
         return SparkSession.builder()
-                .config(conf)
+                .config(SparkConfHelper.withKryo(conf))
                 .getOrCreate();
     }
 }
