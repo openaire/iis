37e885a94a3ce441c3967fd95cad3b9e198d166c
pjacewicz <przemyslaw.jacewicz@icm.edu.pl>
2020-06-23 20:15:31 +0200

    [iis-wf-referenceextraction] tara referenceextraction io utils added

---
+++ iis-wf/iis-wf-referenceextraction/src/main/java/eu/dnetlib/iis/wf/referenceextraction/project/tara/TaraReferenceExtractionIOUtils.java
@@ -0,0 +1,217 @@
+package eu.dnetlib.iis.wf.referenceextraction.project.tara;
+
+import eu.dnetlib.iis.common.cache.CacheMetadataManagingProcess;
+import eu.dnetlib.iis.common.java.io.HdfsUtils;
+import eu.dnetlib.iis.common.lock.LockManager;
+import eu.dnetlib.iis.common.spark.avro.AvroDataFrameSupport;
+import eu.dnetlib.iis.referenceextraction.project.schemas.DocumentHash;
+import eu.dnetlib.iis.referenceextraction.project.schemas.DocumentHashToProject;
+import org.apache.avro.Schema;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class TaraReferenceExtractionIOUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaraReferenceExtractionIOUtils.class);
+
+    private TaraReferenceExtractionIOUtils() {
+    }
+
+    public static void clearOutput(SparkSession spark,
+                                   String outputDocumentToProject) throws IOException {
+        clearOutput(outputDocumentToProject, new OutputCleaner(spark));
+    }
+
+    public static void clearOutput(String outputDocumentToProject,
+                                   OutputCleaner cleaner) throws IOException {
+        logger.info("Clearing output location {}.", outputDocumentToProject);
+        cleaner.clearOutput(outputDocumentToProject);
+    }
+
+    public static Dataset<Row> readDocumentHashToProjectFromCacheOrEmpty(SparkSession spark,
+                                                                         String cacheRootDir,
+                                                                         String existingCacheId) {
+        return readDocumentHashToProjectFromCacheOrEmpty(
+                spark,
+                cacheRootDir,
+                existingCacheId,
+                new AvroDataStoreReader(spark));
+    }
+
+    public static Dataset<Row> readDocumentHashToProjectFromCacheOrEmpty(SparkSession spark,
+                                                                         String cacheRootDir,
+                                                                         String existingCacheId,
+                                                                         AvroDataStoreReader reader) {
+        if (!cacheExist(existingCacheId)) {
+            logger.info("Existing cache id is {}. Returning empty datastore.", existingCacheId);
+            return new AvroDataFrameSupport(spark).createDataFrame(Collections.emptyList(),
+                    DocumentHashToProject.SCHEMA$);
+        }
+        Path existingCache = new Path(cacheRootDir, existingCacheId);
+        logger.info("Existing cache id is {}. Reading datastore from cache path {}.", existingCacheId, existingCache);
+        Path path = new Path(existingCache, CachedTaraReferenceExtractionJob.CacheRecordType.documentHashToProject.name());
+        return reader.read(path.toString(), DocumentHashToProject.SCHEMA$);
+    }
+
+    public static Dataset<Row> readDocumentHashFromCacheOrEmpty(SparkSession spark,
+                                                                String cacheRootDir,
+                                                                String existingCacheId) {
+        return readDocumentHashFromCacheOrEmpty(spark,
+                cacheRootDir,
+                existingCacheId,
+                new AvroDataStoreReader(spark));
+    }
+
+    public static Dataset<Row> readDocumentHashFromCacheOrEmpty(SparkSession spark,
+                                                                String cacheRootDir,
+                                                                String existingCacheId,
+                                                                AvroDataStoreReader reader) {
+        if (!cacheExist(existingCacheId)) {
+            return new AvroDataFrameSupport(spark).createDataFrame(Collections.emptyList(), DocumentHash.SCHEMA$);
+        }
+        Path existingCache = new Path(cacheRootDir, existingCacheId);
+        logger.info("Existing cache id is {}. Reading datastore from cache path {}.", existingCacheId, existingCache);
+        Path path = new Path(existingCache, CachedTaraReferenceExtractionJob.CacheRecordType.documentHash.name());
+        return reader.read(path.toString(), DocumentHash.SCHEMA$);
+    }
+
+    private static Boolean cacheExist(String existingCacheId) {
+        return !CacheMetadataManagingProcess.UNDEFINED.equals(existingCacheId);
+    }
+
+    public static void storeInCache(SparkSession spark,
+                                    Dataset<Row> documentHashToProjectToBeCachedDF,
+                                    Dataset<Row> documentHashToBeCachedDF,
+                                    String cacheRootDir,
+                                    LockManager lockManager,
+                                    CacheMetadataManagingProcess cacheManager,
+                                    int numberOfEmittedFiles) throws Exception {
+        storeInCache(spark,
+                documentHashToProjectToBeCachedDF,
+                documentHashToBeCachedDF,
+                cacheRootDir,
+                lockManager,
+                cacheManager,
+                numberOfEmittedFiles,
+                new AvroDataStoreWriter(spark));
+    }
+
+    public static void storeInCache(SparkSession spark,
+                                    Dataset<Row> documentHashToProjectToBeCachedDF,
+                                    Dataset<Row> documentHashToBeCachedDF,
+                                    String cacheRootDir,
+                                    LockManager lockManager,
+                                    CacheMetadataManagingProcess cacheManager,
+                                    int numberOfEmittedFiles,
+                                    AvroDataStoreWriter writer) throws Exception {
+        lockManager.obtain(cacheRootDir);
+        try {
+            String newCacheId = cacheManager.generateNewCacheId(spark.sparkContext().hadoopConfiguration(), cacheRootDir);
+            Path newCachePath = new Path(cacheRootDir, newCacheId);
+            FileSystem fileSystem = FileSystem.get(spark.sparkContext().hadoopConfiguration());
+            logger.info("Storing cached data in path {}.", newCachePath.toString());
+            try {
+                storeDocumentHashToProjectInCache(documentHashToProjectToBeCachedDF,
+                        newCachePath,
+                        numberOfEmittedFiles,
+                        writer);
+                storeDocumentHashInCache(documentHashToBeCachedDF,
+                        newCachePath,
+                        numberOfEmittedFiles,
+                        writer);
+                storeMetaInCache(cacheManager,
+                        cacheRootDir,
+                        newCacheId,
+                        spark.sparkContext().hadoopConfiguration());
+            } catch (Exception e) {
+                fileSystem.delete(newCachePath, true);
+                throw e;
+            }
+        } finally {
+            lockManager.release(cacheRootDir);
+        }
+    }
+
+    private static void storeDocumentHashToProjectInCache(Dataset<Row> documentHashToProjectToBeCachedDF,
+                                                          Path newCachePath,
+                                                          int numberOfEmittedFiles,
+                                                          AvroDataStoreWriter writer) {
+        writer.write(documentHashToProjectToBeCachedDF.coalesce(numberOfEmittedFiles),
+                new Path(newCachePath,
+                        CachedTaraReferenceExtractionJob.CacheRecordType.documentHashToProject.name()).toString());
+    }
+
+    private static void storeDocumentHashInCache(Dataset<Row> documentHashToBeCachedDF,
+                                                 Path newCachePath,
+                                                 int numberOfEmittedFiles,
+                                                 AvroDataStoreWriter writer) {
+        writer.write(documentHashToBeCachedDF.coalesce(numberOfEmittedFiles),
+                new Path(newCachePath,
+                        CachedTaraReferenceExtractionJob.CacheRecordType.documentHash.name()).toString());
+    }
+
+    private static void storeMetaInCache(CacheMetadataManagingProcess cacheManager,
+                                         String cacheRootDir,
+                                         String newCacheId,
+                                         Configuration conf) throws IOException {
+        cacheManager.writeCacheId(conf, cacheRootDir, newCacheId);
+    }
+
+    public static void storeInOutput(SparkSession spark,
+                                     Dataset<Row> resultsToOutputDF,
+                                     String outputDocumentToProject) {
+        storeInOutput(resultsToOutputDF, outputDocumentToProject, new AvroDataStoreWriter(spark));
+    }
+
+    public static void storeInOutput(Dataset<Row> resultsToOutputDF,
+                                     String outputDocumentToProject,
+                                     AvroDataStoreWriter writer) {
+        logger.info("Storing output data in path {}.", outputDocumentToProject);
+        writer.write(resultsToOutputDF, outputDocumentToProject);
+    }
+
+    public static class OutputCleaner {
+        private Configuration conf;
+
+        public OutputCleaner(SparkSession spark) {
+            this.conf = spark.sparkContext().hadoopConfiguration();
+        }
+
+        public void clearOutput(String output) throws IOException {
+            HdfsUtils.remove(conf, output);
+        }
+    }
+
+    public static class AvroDataStoreReader {
+        private AvroDataFrameSupport avroDataFrameSupport;
+
+        private AvroDataStoreReader(SparkSession spark) {
+            this.avroDataFrameSupport = new AvroDataFrameSupport(spark);
+        }
+
+        public Dataset<Row> read(String path, Schema schema) {
+            return avroDataFrameSupport.read(path, schema);
+        }
+    }
+
+    public static class AvroDataStoreWriter {
+        private AvroDataFrameSupport avroDataFrameSupport;
+
+        public AvroDataStoreWriter(SparkSession spark) {
+            this.avroDataFrameSupport = new AvroDataFrameSupport(spark);
+        }
+
+        public void write(Dataset<Row> df, String path) {
+            avroDataFrameSupport.write(df, path);
+        }
+    }
+}
---
+++ iis-wf/iis-wf-referenceextraction/src/test/java/eu/dnetlib/iis/wf/referenceextraction/project/tara/TaraReferenceExtractionIOUtilsTest.java
@@ -0,0 +1,238 @@
+package eu.dnetlib.iis.wf.referenceextraction.project.tara;
+
+import eu.dnetlib.iis.common.cache.CacheMetadataManagingProcess;
+import eu.dnetlib.iis.common.lock.LockManager;
+import eu.dnetlib.iis.common.spark.avro.AvroDataFrameSupport;
+import eu.dnetlib.iis.referenceextraction.project.schemas.DocumentHash;
+import eu.dnetlib.iis.referenceextraction.project.schemas.DocumentHashToProject;
+import eu.dnetlib.iis.referenceextraction.project.schemas.DocumentToProject;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import static eu.dnetlib.iis.wf.referenceextraction.project.tara.TaraReferenceExtractionIOUtils.*;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+public class TaraReferenceExtractionIOUtilsTest {
+
+    private static SparkSession spark;
+
+    @BeforeClass
+    public static void beforeClass() {
+        SparkConf conf = new SparkConf();
+        conf.setMaster("local");
+        conf.set("spark.driver.host", "localhost");
+        conf.setAppName(TaraReferenceExtractionIOUtilsTest.class.getSimpleName());
+        spark = SparkSession.builder().config(conf).getOrCreate();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        spark.stop();
+    }
+
+    @Test
+    public void clearOutputShouldRunProperly() throws IOException {
+        // given
+        OutputCleaner cleaner = mock(OutputCleaner.class);
+
+        // when
+        clearOutput("path/to/output", cleaner);
+
+        // then
+        verify(cleaner).clearOutput("path/to/output");
+    }
+
+    @Test
+    public void readDocumentHashToProjectFromCacheOrEmptyShouldReadEmptyDataFrameIfExistingCacheIdIsUndefined() {
+        // given
+        AvroDataStoreReader reader = mock(AvroDataStoreReader.class);
+
+        // when
+        List<Row> results = readDocumentHashToProjectFromCacheOrEmpty(spark,
+                "path/to/cache",
+                CacheMetadataManagingProcess.UNDEFINED,
+                reader)
+                .collectAsList();
+
+        // then
+        assertTrue(results.isEmpty());
+        verify(reader, never()).read(any(), any());
+    }
+
+    @Test
+    public void readDocumentHashToProjectFromCacheOrEmptyShouldReadNonEmptyDataFraneIfExistingCacheIdIsDefined() {
+        // given
+        DocumentHashToProject documentHashToProject = DocumentHashToProject.newBuilder()
+                .setHashValue(1)
+                .setProjectId("projectId")
+                .setConfidenceLevel(1.0f)
+                .build();
+        List<DocumentHashToProject> documentHashToProjectList = Collections.singletonList(documentHashToProject);
+        AvroDataStoreReader reader = mock(AvroDataStoreReader.class);
+        when(reader.read(new Path("path/to/cache/01",
+                        CachedTaraReferenceExtractionJob.CacheRecordType.documentHashToProject.name()).toString(),
+                DocumentHashToProject.SCHEMA$))
+                .thenReturn(new AvroDataFrameSupport(spark).createDataFrame(documentHashToProjectList,
+                        DocumentHashToProject.SCHEMA$));
+
+        // when
+        List<Row> results = readDocumentHashToProjectFromCacheOrEmpty(spark,
+                "path/to/cache",
+                "01",
+                reader)
+                .collectAsList();
+
+        // then
+        assertEquals(1, results.size());
+        Row row = results.get(0);
+        assertEquals(documentHashToProject.getHashValue(), row.getAs("hashValue"));
+        assertEquals(documentHashToProject.getProjectId(), row.getAs("projectId"));
+        assertEquals(documentHashToProject.getConfidenceLevel(), row.<Float>getAs("confidenceLevel"), 1e-3);
+        assertNull(row.getAs("textsnippet"));
+    }
+
+    @Test
+    public void readDocumentHashFromCacheOrEmptyShouldReadEmptyDataFrameIfExistingCacheIdIsUndefined() {
+        // given
+        AvroDataStoreReader reader = mock(AvroDataStoreReader.class);
+
+        // when
+        List<Row> results = readDocumentHashFromCacheOrEmpty(spark,
+                "path/to/cache",
+                CacheMetadataManagingProcess.UNDEFINED,
+                reader)
+                .collectAsList();
+
+        // then
+        assertTrue(results.isEmpty());
+        verify(reader, never()).read(any(), any());
+    }
+
+    @Test
+    public void readDocumentHashFromCacheOrEmptyShouldReadNonEmptyDataFrameIfExistingCacheIdIsDefined() {
+        // given
+        DocumentHash documentHash = DocumentHash.newBuilder().setHashValue(1).build();
+        List<DocumentHash> documentHashList = Collections.singletonList(documentHash);
+        AvroDataStoreReader reader = mock(AvroDataStoreReader.class);
+        when(reader.read(new Path("path/to/cache/01", CachedTaraReferenceExtractionJob.CacheRecordType.documentHash.name()).toString(),
+                DocumentHash.SCHEMA$))
+                .thenReturn(new AvroDataFrameSupport(spark).createDataFrame(documentHashList, DocumentHash.SCHEMA$));
+
+        // when
+        List<Row> results = readDocumentHashFromCacheOrEmpty(spark,
+                "path/to/cache",
+                "01",
+                reader)
+                .collectAsList();
+
+        // then
+        assertEquals(1, results.size());
+        Row row = results.get(0);
+        assertEquals(documentHash.getHashValue(), row.getAs("hashValue"));
+    }
+
+    @Test
+    public void storeInCacheShouldRunProperly() throws Exception {
+        // given
+        AvroDataFrameSupport avroDataFrameSupport = new AvroDataFrameSupport(spark);
+        DocumentHashToProject documentHashToProject = DocumentHashToProject.newBuilder()
+                .setHashValue(1)
+                .setProjectId("projId-1")
+                .setConfidenceLevel(1.0f)
+                .build();
+        List<DocumentHashToProject> documentHashToProjectList = Collections.singletonList(documentHashToProject);
+        Dataset<Row> documentHashToProjectDF = avroDataFrameSupport.createDataFrame(
+                documentHashToProjectList,
+                DocumentHashToProject.SCHEMA$);
+        DocumentHash documentHash = DocumentHash.newBuilder().setHashValue(1).build();
+        List<DocumentHash> documentHashList = Collections.singletonList(documentHash);
+        Dataset<Row> documentHashDF = avroDataFrameSupport.createDataFrame(
+                documentHashList,
+                DocumentHash.SCHEMA$);
+        LockManager lockManager = mock(LockManager.class);
+        CacheMetadataManagingProcess cacheManager = mock(CacheMetadataManagingProcess.class);
+        when(cacheManager.generateNewCacheId(any(Configuration.class), eq("path/to/cache"))).thenReturn("01");
+        AvroDataStoreWriter writer = mock(AvroDataStoreWriter.class);
+
+        // when
+        storeInCache(spark,
+                documentHashToProjectDF,
+                documentHashDF,
+                "path/to/cache",
+                lockManager,
+                cacheManager,
+                10,
+                writer);
+
+        // then
+        ArgumentCaptor<Dataset<Row>> dataFrameCaptor = new ArgumentCaptor<>();
+
+        verify(lockManager, atLeastOnce()).obtain("path/to/cache");
+
+        verify(writer, atLeastOnce()).write(dataFrameCaptor.capture(),
+                eq(String.format("path/to/cache/01/%s",
+                        CachedTaraReferenceExtractionJob.CacheRecordType.documentHashToProject.name())));
+        List<Row> documentHashToProjectRows = dataFrameCaptor.getValue().collectAsList();
+        assertEquals(1, documentHashToProjectRows.size());
+        Row documentHashToProjectRow = documentHashToProjectRows.get(0);
+        assertEquals(documentHashToProject.getHashValue(), documentHashToProjectRow.getAs("hashValue"));
+        assertEquals(documentHashToProject.getProjectId(), documentHashToProjectRow.getAs("projectId"));
+        assertEquals(documentHashToProject.getConfidenceLevel(), documentHashToProjectRow.<Float>getAs("confidenceLevel"), 1e-3);
+        assertNull(documentHashToProjectRow.getAs("textsnippet"));
+
+        verify(writer, atLeastOnce()).write(dataFrameCaptor.capture(),
+                eq(String.format("path/to/cache/01/%s",
+                        CachedTaraReferenceExtractionJob.CacheRecordType.documentHash.name())));
+        List<Row> documentHashRows = dataFrameCaptor.getValue().collectAsList();
+        assertEquals(1, documentHashRows.size());
+        Row documentHashRow = documentHashRows.get(0);
+        assertEquals(documentHash.getHashValue(), documentHashRow.getAs("hashValue"));
+
+        verify(cacheManager, atLeastOnce()).writeCacheId(any(Configuration.class), eq("path/to/cache"), eq("01"));
+        verify(lockManager, atLeastOnce()).release("path/to/cache");
+    }
+
+    @Test
+    public void storeInOutputShouldRunProperly() {
+        // given
+        DocumentToProject documentToProject = DocumentToProject.newBuilder()
+                .setDocumentId("docId-1")
+                .setProjectId("projId-1")
+                .setConfidenceLevel(1.0f)
+                .build();
+        List<DocumentToProject> documentToProjectList = Collections.singletonList(documentToProject);
+        Dataset<Row> documentToProjectDF = new AvroDataFrameSupport(spark).createDataFrame(
+                documentToProjectList,
+                DocumentToProject.SCHEMA$);
+        AvroDataStoreWriter writer = mock(AvroDataStoreWriter.class);
+
+        // when
+        storeInOutput(documentToProjectDF, "path/to/output", writer);
+
+        // then
+        ArgumentCaptor<Dataset<Row>> dataFrameCaptor = new ArgumentCaptor<>();
+        verify(writer, atLeastOnce()).write(dataFrameCaptor.capture(),
+                eq("path/to/output"));
+        List<Row> documentToProjectRows = dataFrameCaptor.getValue().collectAsList();
+        assertEquals(1, documentToProjectRows.size());
+        Row documentToProjecRow = documentToProjectRows.get(0);
+        assertEquals(documentToProject.getDocumentId(), documentToProjecRow.getAs("documentId"));
+        assertEquals(documentToProject.getProjectId(), documentToProjecRow.getAs("projectId"));
+        assertEquals(documentToProject.getConfidenceLevel(), documentToProjecRow.<Float>getAs("confidenceLevel"), 1e-3);
+        assertNull(documentToProjecRow.getAs("textsnippet"));
+    }
+}
