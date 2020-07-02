b9f5edff21d83504972a758af7da26d1ebf6bbf7
pjacewicz <przemyslaw.jacewicz@icm.edu.pl>
2020-07-02 14:19:38 +0200

    [iis-wf-referenceextraction] class structure changes and doc added

--- iis-wf/iis-wf-referenceextraction/src/main/java/eu/dnetlib/iis/wf/referenceextraction/project/tara/TaraPipeExecutionEnvironment.java
+++ iis-wf/iis-wf-referenceextraction/src/main/java/eu/dnetlib/iis/wf/referenceextraction/project/tara/TaraPipeExecutionEnvironment.java
@@ -3,21 +3,22 @@ package eu.dnetlib.iis.wf.referenceextraction.project.tara;
 import eu.dnetlib.iis.common.spark.pipe.PipeExecutionEnvironment;
 import org.apache.spark.SparkContext;
 
+/**
+ * Abstraction of cluster execution environment for TARA project reference extraction.
+ * <p>
+ * Madis scripts dir and SQLite projects db file will be propagated to cluster worker nodes upon creation  of this
+ * class. String command to be run by RDD 'pipe' method executes TARA reference extraction in the context of each
+ * node's worker dir.
+ */
 public class TaraPipeExecutionEnvironment implements PipeExecutionEnvironment {
-    private SparkContext sc;
-    private String scriptsDir;
-    private String projectDbFile;
 
     public TaraPipeExecutionEnvironment(SparkContext sc, String scriptsDir, String projectDbFile) {
-        this.sc = sc;
-        this.scriptsDir = scriptsDir;
-        this.projectDbFile = projectDbFile;
+        sc.addFile(scriptsDir, true);
+        sc.addFile(projectDbFile);
     }
 
     @Override
     public String pipeCommand() {
-        sc.addFile(scriptsDir, true);
-        sc.addFile(projectDbFile);
         return "bash scripts/run_tara_referenceextraction.sh";
     }
 }
