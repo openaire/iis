ab8a10b54ea709e4a11c690939cb35b7fcdf3dcf
Przemysław Jacewicz <przemyslaw.jacewicz@gmail.com>
2020-09-24 18:14:49 +0200

    Closes #1102: Drop redundant delete operations from workflow definitions of spark actions

--- iis-wf/iis-wf-transformers/src/main/java/eu/dnetlib/iis/wf/transformers/avro2json/Avro2JsonTransformer.java
+++ iis-wf/iis-wf-transformers/src/main/java/eu/dnetlib/iis/wf/transformers/avro2json/Avro2JsonTransformer.java
@@ -3,12 +3,15 @@ package eu.dnetlib.iis.wf.transformers.avro2json;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import eu.dnetlib.iis.common.java.io.HdfsUtils;
 import eu.dnetlib.iis.common.spark.JavaSparkContextFactory;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
+
+import java.io.IOException;
 
 /**
  * Spark job transforming Avro records into Json records.
@@ -19,12 +22,14 @@ public class Avro2JsonTransformer {
 
     //------------------------ LOGIC --------------------------
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         AvroToRdbTransformerJobParameters params = new AvroToRdbTransformerJobParameters();
         JCommander jcommander = new JCommander(params);
         jcommander.parse(args);
         
         try (JavaSparkContext sc = JavaSparkContextFactory.withConfAndKryo(new SparkConf())) {
+            HdfsUtils.remove(sc.hadoopConfiguration(), params.output);
+
             SQLContext sqlContext = new SQLContext(sc);
             Dataset<Row> input = sqlContext.read().format("com.databricks.spark.avro").load(params.input);
             input.write().json(params.output);
