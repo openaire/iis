0a88884ad45b199909dc212c73009011f172c7ab
Przemysław Jacewicz <przemyslaw.jacewicz@gmail.com>
2020-09-29 13:22:17 +0200

    Closes #1151: Introduce a common functionality for retrieving resources
    Increasing timeout for software ref extraction workflow test

--- iis-wf/iis-wf-referenceextraction/src/test/java/eu/dnetlib/iis/wf/referenceextraction/softwareurl/SoftwareUrlRefExtractionWfTest.java
+++ iis-wf/iis-wf-referenceextraction/src/test/java/eu/dnetlib/iis/wf/referenceextraction/softwareurl/SoftwareUrlRefExtractionWfTest.java
@@ -1,5 +1,6 @@
 package eu.dnetlib.iis.wf.referenceextraction.softwareurl;
 
+import eu.dnetlib.iis.common.OozieWorkflowTestConfiguration;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -15,17 +16,23 @@ import eu.dnetlib.iis.common.IntegrationTest;
 public class SoftwareUrlRefExtractionWfTest extends AbstractOozieWorkflowTestCase {
 
     @Test
-    public void testMainWorkflow() throws Exception {
-        testWorkflow("eu/dnetlib/iis/wf/referenceextraction/softwareurl/main/sampletest");
+    public void testMainWorkflow() {
+        OozieWorkflowTestConfiguration configuration = new OozieWorkflowTestConfiguration();
+        configuration.setTimeoutInSeconds(1800);
+        testWorkflow("eu/dnetlib/iis/wf/referenceextraction/softwareurl/main/sampletest", configuration);
     }
 
     @Test
-    public void testMainWorkflowWithoutReferences() throws Exception {
+    public void testMainWorkflowWithoutReferences() {
+        OozieWorkflowTestConfiguration configuration = new OozieWorkflowTestConfiguration();
+        configuration.setTimeoutInSeconds(1800);
         testWorkflow("eu/dnetlib/iis/wf/referenceextraction/softwareurl/main/sampletest_without_references");
     }
 
     @Test
-    public void testMainWorkflowEmptyInput() throws Exception {
+    public void testMainWorkflowEmptyInput() {
+        OozieWorkflowTestConfiguration configuration = new OozieWorkflowTestConfiguration();
+        configuration.setTimeoutInSeconds(1800);
         testWorkflow("eu/dnetlib/iis/wf/referenceextraction/softwareurl/main/sampletest_empty_input");
     }
 }
