cb700f93f9c09e9fa64a5751b872d10b40be342f
Marek Horst <mhorst@icm.edu.pl>
2020-10-05 17:03:01 +0200

    Closes #1157: Apply text-snippet improvements to the funders mining
    Introducing AoF fix in projects.sql mining script.

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/project/main_sqlite/oozie_app/lib/scripts/projects.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/project/main_sqlite/oozie_app/lib/scripts/projects.sql
@@ -253,7 +253,7 @@ create temp table secondary_output_table as
 select jdict('documentId', docid, 'projectId', id, 'confidenceLevel', sqroot(min(1.49,confidence)/1.5), 'textsnippet', context) as C1, docid, id, fundingclass1, grantid, context from ( select docid,id,confidence, docid, id,  fundingclass1, grantid, context from ( select
 0.8 as confidence, docid, id, fundingclass1, grantid, context
 from (
-unindexed select docid, regexpr("(\d+)",middle) as middle, (prev1||" "||prev2||" "||prev3||" "||prev4||" "||prev5||" "||prev6||" "||prev7||" "||prev8||" "||prev9||" "||prev10||" "||prev11||" "||prev12||" "||prev13||" <<< "||middle||" >>> "||next) as context
+unindexed select docid, regexpr("(\d+)",middle) as middle, comprspaces(j2s(prev1,prev2,prev3,prev4,prev5,prev6,prev7,prev8,prev9,prev10,prev11,prev12,prev13,"<<<",middle,">>>",next)) as context
 from (
   setschema 'docid,prev1,prev2,prev3,prev4,prev5,prev6,prev7,prev8,prev9,prev10,prev11,prev12,prev13,middle,next' select c1, textwindow(lower(c2),-13,0,1, '\b\d{5,6}\b') from pubs where c2 is not null
 ) where CAST(regexpr("(\d+)",middle) AS INT)>70000), grants
