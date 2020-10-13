a63e2e9951081d5e417fa9143109a2a4491c6e30
johnfouf <johnfouf@gmail.com>
2020-10-13 17:38:51 +0300

    Closes #1171: Fix "eval disabled" error while building opentrials sqlite database
    Solves 'eval disabled error' just solving it at the moment, a different strategy will be committed soon.

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/dataset/opentrials/sqlite_builder/oozie_app/lib/scripts/buildopentrialsdb.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/dataset/opentrials/sqlite_builder/oozie_app/lib/scripts/buildopentrialsdb.sql
@@ -1,6 +1,9 @@
 drop table if exists opentrials;
 
-create table opentrials as select id,acronym from (select id,publisher,case when alternateidentifiers is null then "" else jsonpath(alternateidentifiers,publisher) end as acronym from (setschema 'id,publisher,alternateIdentifiers'select jdictsplit(c1,"id","publisher","alternateIdentifiers") from stdinput())) ;
+create table opentrials as select id,acronym from 
+(select id,publisher,case when alternateidentifiers is null then "" 
+    else jsonpath(alternateidentifiers, publisher) end as acronym 
+        from (setschema 'id,publisher,alternateIdentifiers' select jdictsplit(c1,"id","publisher","alternateIdentifiers") from stdinput())) ;
 
 create index opentrialsindex on opentrials(acronym,id);
 
