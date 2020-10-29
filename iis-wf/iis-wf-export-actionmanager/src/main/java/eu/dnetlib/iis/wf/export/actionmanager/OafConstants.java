3769453be8cd0afce1594d5e9c0142daa72d5bcf
pjacewicz <przemyslaw.jacewicz@icm.edu.pl>
2020-10-29 14:57:40 +0100

    [iis-wf-export-actionmanager] adding constants for exporting citation matching outcome as oaf relations

--- iis-wf/iis-wf-export-actionmanager/src/main/java/eu/dnetlib/iis/wf/export/actionmanager/OafConstants.java
+++ iis-wf/iis-wf-export-actionmanager/src/main/java/eu/dnetlib/iis/wf/export/actionmanager/OafConstants.java
@@ -10,13 +10,16 @@ public class OafConstants {
     public static final String SUBREL_TYPE_PUBLICATION_DATASET = "publicationDataset";
     public static final String SUBREL_TYPE_OUTCOME = "outcome";
     public static final String SUBREL_TYPE_AFFILIATION = "affiliation";
-    
+    public static final String SUBREL_TYPE_CITATION = "citation";
+
     public static final String REL_CLASS_ISRELATEDTO = "isRelatedTo";
     public static final String REL_CLASS_ISPRODUCEDBY = "isProducedBy";
     public static final String REL_CLASS_PRODUCES = "produces";
     public static final String REL_CLASS_HAS_AUTHOR_INSTITUTION_OF = "hasAuthorInstitution";
     public static final String REL_CLASS_IS_AUTHOR_INSTITUTION_OF = "isAuthorInstitutionOf";
-    
+    public static final String REL_CLASS_CITES = "cites";
+    public static final String REL_CLASS_ISCITEDBY = "IsCitedBy";
+
     private OafConstants() {}
     
 }
