1cc94ff18a25e3c5755e9a3b17f9cb947f231fa9
pjacewicz <przemyslaw.jacewicz@icm.edu.pl>
2020-10-29 12:30:26 +0100

    [iis-wf-common] adding support for test using local spark session

---
+++ iis-common/src/test/java/eu/dnetlib/iis/common/spark/TestWithSharedSparkSession.java
@@ -0,0 +1,38 @@
+package eu.dnetlib.iis.common.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Objects;
+
+/**
+ * Support for tests using {@link SparkSession} - extends this class to access SparkSession in local mode.
+ */
+public class TestWithSharedSparkSession {
+    private static transient SparkSession _spark;
+    protected boolean initialized = false;
+
+    public SparkSession spark() {
+        return _spark;
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        initialized = Objects.nonNull(_spark);
+
+        if (!initialized) {
+            SparkConf conf = new SparkConf();
+            conf.setMaster("local");
+            conf.set("spark.driver.host", "localhost");
+            _spark = SparkSession.builder().config(conf).getOrCreate();
+        }
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        _spark.stop();
+        _spark = null;
+    }
+}
