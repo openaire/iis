3c08d8f7e3f6d10a74df49fe01005746b0c39169
Przemysław Jacewicz <przemyslaw.jacewicz@icm.edu.pl>
2021-05-17 09:16:15 +0200

    updating import information space job utils

--- iis-wf/iis-wf-import/src/main/java/eu/dnetlib/iis/wf/importer/infospace/ImportInformationSpaceJobUtils.java
+++ iis-wf/iis-wf-import/src/main/java/eu/dnetlib/iis/wf/importer/infospace/ImportInformationSpaceJobUtils.java
@@ -4,10 +4,7 @@ import eu.dnetlib.iis.common.schemas.IdentifierMapping;
 import eu.dnetlib.iis.common.spark.avro.AvroDataFrameSupport;
 import eu.dnetlib.iis.common.spark.avro.AvroDatasetSupport;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 
 import static org.apache.spark.sql.functions.*;
 
@@ -21,8 +18,12 @@ public class ImportInformationSpaceJobUtils {
     }
 
     /**
-     * Substitutes original ids build from relations with object store ids build from {@link eu.dnetlib.dhp.schema.oaf.OafEntity#originalId}
-     * field for records that extend {@link eu.dnetlib.dhp.schema.oaf.Result}.
+     * Produces a mapping from graph id to object store id by joining together a mapping from deduplicated ids into
+     * original ids and a mapping for records that extend {@link eu.dnetlib.dhp.schema.oaf.Result} type.
+     * <p>
+     * For {@link eu.dnetlib.dhp.schema.oaf.Result} records the object store id is build from
+     * {@link eu.dnetlib.dhp.schema.oaf.Result#originalId} field. For other entities the object store id is a
+     * pre-deduplication id.
      * <p>
      * NOTE: this implementation servers as a temporary solution and should be unnecessary when the graph and object store
      * will be using the same ids.
@@ -33,15 +34,14 @@ public class ImportInformationSpaceJobUtils {
      * @param sourcePublication          {@link eu.dnetlib.dhp.schema.oaf.Publication} RDD.
      * @param sourceSoftware             {@link eu.dnetlib.dhp.schema.oaf.Software} RDD.
      * @param spark                      Instance of SparkSession.
-     * @return An RDD of {@link IdentifierMapping} with original ids of {@link eu.dnetlib.dhp.schema.oaf.Result} records
-     * replaced.
+     * @return An RDD of {@link IdentifierMapping} with object store ids.
      */
-    public static JavaRDD<IdentifierMapping> mapToObjectStoreId(JavaRDD<IdentifierMapping> dedupRelation,
-                                                                JavaRDD<eu.dnetlib.dhp.schema.oaf.Dataset> sourceDataset,
-                                                                JavaRDD<eu.dnetlib.dhp.schema.oaf.OtherResearchProduct> sourceOtherResearchProduct,
-                                                                JavaRDD<eu.dnetlib.dhp.schema.oaf.Publication> sourcePublication,
-                                                                JavaRDD<eu.dnetlib.dhp.schema.oaf.Software> sourceSoftware,
-                                                                SparkSession spark) {
+    public static JavaRDD<IdentifierMapping> produceObjectStoreId(JavaRDD<IdentifierMapping> dedupRelation,
+                                                                  JavaRDD<eu.dnetlib.dhp.schema.oaf.Dataset> sourceDataset,
+                                                                  JavaRDD<eu.dnetlib.dhp.schema.oaf.OtherResearchProduct> sourceOtherResearchProduct,
+                                                                  JavaRDD<eu.dnetlib.dhp.schema.oaf.Publication> sourcePublication,
+                                                                  JavaRDD<eu.dnetlib.dhp.schema.oaf.Software> sourceSoftware,
+                                                                  SparkSession spark) {
         Dataset<eu.dnetlib.dhp.schema.oaf.Dataset> sourceDatasetDS = spark.createDataset(
                 sourceDataset.rdd(), Encoders.bean(eu.dnetlib.dhp.schema.oaf.Dataset.class));
         Dataset<eu.dnetlib.dhp.schema.oaf.OtherResearchProduct> sourceOtherResearchProductDS = spark.createDataset(
@@ -51,20 +51,31 @@ public class ImportInformationSpaceJobUtils {
         Dataset<eu.dnetlib.dhp.schema.oaf.Software> sourceSoftwareDS = spark.createDataset(
                 sourceSoftware.rdd(), Encoders.bean(eu.dnetlib.dhp.schema.oaf.Software.class));
 
-        org.apache.spark.sql.Dataset<Row> resultIdMapDF = sourceDatasetDS.select(col("id"), explode(col("originalId")).as("oid"))
+        Dataset<Row> resultIdMapDF = sourceDatasetDS.select(col("id"), explode(col("originalId")).as("oid"))
                 .union(sourceOtherResearchProductDS.select(col("id"), explode(col("originalId")).as("oid")))
                 .union(sourcePublicationDS.select(col("id"), explode(col("originalId")).as("oid")))
                 .union(sourceSoftwareDS.select(col("id"), explode(col("originalId")).as("oid")))
+                .where(not(col("id").like("%dedup%")))
+                .where(col("oid").like("50|%"))
                 .distinct();
 
-        org.apache.spark.sql.Dataset<Row> dedupRelationDF = new AvroDatasetSupport(spark).toDF(
+        Dataset<Row> dedupRelationDF = new AvroDatasetSupport(spark).toDF(
                 spark.createDataset(dedupRelation.rdd(), Encoders.kryo(IdentifierMapping.class)), IdentifierMapping.SCHEMA$);
 
-        Dataset<Row> dedupRelationWithOidDF = dedupRelationDF
-                .join(resultIdMapDF, col("originalId").equalTo(col("id")), "left_outer")
-                .select(col("newId"), when(col("oid").isNotNull(), col("oid"))
-                        .otherwise(col("originalId")).as("originalId"));
+        Column dedupMappingNotMatched = col("newId").isNull().and(col("id").isNotNull());
+        Column resultMappingNotMatched = col("newId").isNotNull().and(col("id").isNull());
 
-        return new AvroDataFrameSupport(spark).toDS(dedupRelationWithOidDF, IdentifierMapping.class).toJavaRDD();
+        Dataset<Row> identifierMappingDF = dedupRelationDF
+                .join(resultIdMapDF, col("originalId").equalTo(col("id")), "full_outer")
+                .select(
+                        when(dedupMappingNotMatched, col("id"))
+                                .when(resultMappingNotMatched, col("newId"))
+                                .otherwise(col("newId")).as("newId"),
+                        when(dedupMappingNotMatched, col("oid"))
+                                .when(resultMappingNotMatched, col("originalId"))
+                                .otherwise(col("oid")).as("originalId")
+                );
+
+        return new AvroDataFrameSupport(spark).toDS(identifierMappingDF, IdentifierMapping.class).toJavaRDD();
     }
 }
--- iis-wf/iis-wf-import/src/test/java/eu/dnetlib/iis/wf/importer/infospace/ImportInformationSpaceJobUtilsTest.java
+++ iis-wf/iis-wf-import/src/test/java/eu/dnetlib/iis/wf/importer/infospace/ImportInformationSpaceJobUtilsTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -30,47 +29,84 @@ public class ImportInformationSpaceJobUtilsTest extends TestWithSharedSparkSessi
     }
 
     @Nested
-    public class MapToObjectStoreIdTest {
+    public class ProduceObjectStoreIdTest {
 
         @Test
-        @DisplayName("Result record ids are mapped to object store ids")
-        public void givenIdentifierMappingsAndResultRecords_whenMappedToObjectStoreIds_thenProperIdentifierMappingsAreSubstituted() {
-            List<IdentifierMapping> identifierMappings = Arrays.asList(
-                    createIdentifierMapping("dataset-new-id-1", "dataset-original-id-1"),
-                    createIdentifierMapping("organization-new-id-1", "organization-original-id-1"),
-                    createIdentifierMapping("orp-new-id-1", "orp-original-id-1"),
-                    createIdentifierMapping("project-new-id-1", "project-original-id-1"),
-                    createIdentifierMapping("publication-new-id-1", "publication-original-id-1"),
-                    createIdentifierMapping("software-new-id-1", "software-original-id-1"));
-            JavaRDD<IdentifierMapping> dedupRelation = jsc.parallelize(identifierMappings);
+        @DisplayName("Dedup relations and result records produce object store ids")
+        public void givenDedupRelationsAndResultRecords_whenObjectStoreIdsAreProduced_thenProperIdentifierMappingsAreCreated() {
+            JavaRDD<IdentifierMapping> dedupRelation = jsc.parallelize(
+                    Arrays.asList(
+                            createIdentifierMapping("dataset-new-id-1", "dataset-original-id-1"),
+                            createIdentifierMapping("dataset-new-id-2", "dataset-original-id-2"),
+                            createIdentifierMapping("organization-new-id-1", "organization-original-id-1"),
+                            createIdentifierMapping("orp-new-id-1", "orp-original-id-1"),
+                            createIdentifierMapping("orp-new-id-2", "orp-original-id-2"),
+                            createIdentifierMapping("project-new-id-1", "project-original-id-1"),
+                            createIdentifierMapping("publication-new-id-1", "publication-original-id-1"),
+                            createIdentifierMapping("publication-new-id-2", "publication-original-id-2"),
+                            createIdentifierMapping("software-new-id-1", "software-original-id-1"),
+                            createIdentifierMapping("software-new-id-2", "software-original-id-2"))
+            );
 
             JavaRDD<Dataset> sourceDataset = jsc.parallelize(
-                    Collections.singletonList(createResultEntity(Dataset::new, "dataset-original-id-1",
-                            "dataset-objectstore-id-1", "dataset-objectstore-id-1")));
+                    Arrays.asList(
+                            createResultEntity(Dataset::new, "dataset-original-id-1",
+                                    "50|dataset-objectstore-id-1", "50|dataset-objectstore-id-1"),
+                            createResultEntity(Dataset::new, "dataset-new-id-3",
+                                    "50|dataset-objectstore-id-3", "50|dataset-objectstore-id-3"),
+                            createResultEntity(Dataset::new, "dataset-dedup-id-4",
+                                    "50|dataset-objectstore-id-4", "50|dataset-objectstore-id-4")
+                    ));
             JavaRDD<OtherResearchProduct> sourceOtherResearchProduct = jsc.parallelize(
-                    Collections.singletonList(createResultEntity(OtherResearchProduct::new, "orp-original-id-1",
-                            "orp-objectstore-id-1", "orp-objectstore-id-1")));
+                    Arrays.asList(
+                            createResultEntity(OtherResearchProduct::new, "orp-original-id-1",
+                                    "50|orp-objectstore-id-1", "50|orp-objectstore-id-1"),
+                            createResultEntity(OtherResearchProduct::new, "orp-new-id-3",
+                                    "50|orp-objectstore-id-3", "50|orp-objectstore-id-3"),
+                            createResultEntity(OtherResearchProduct::new, "orp-dedup-id-4",
+                                    "50|orp-objectstore-id-4", "50|orp-objectstore-id-4")
+                    ));
             JavaRDD<Publication> sourcePublication = jsc.parallelize(
-                    Collections.singletonList(createResultEntity(Publication::new, "publication-original-id-1",
-                            "publication-objectstore-id-1", "publication-objectstore-id-1")));
+                    Arrays.asList(
+                            createResultEntity(Publication::new, "publication-original-id-1",
+                                    "50|publication-objectstore-id-1", "50|publication-objectstore-id-1"),
+                            createResultEntity(Publication::new, "publication-new-id-3",
+                                    "50|publication-objectstore-id-3", "50|publication-objectstore-id-3"),
+                            createResultEntity(Publication::new, "publication-dedup-id-4",
+                                    "50|publication-objectstore-id-4", "50|publication-objectstore-id-4")
+                    ));
             JavaRDD<Software> sourceSoftware = jsc.parallelize(
-                    Collections.singletonList(createResultEntity(Software::new, "software-original-id-1",
-                            "software-objectstore-id-1", "software-objectstore-id-1")));
+                    Arrays.asList(
+                            createResultEntity(Software::new, "software-original-id-1",
+                                    "50|software-objectstore-id-1", "50|software-objectstore-id-1"),
+                            createResultEntity(Software::new, "software-new-id-3",
+                                    "50|software-objectstore-id-3", "50|software-objectstore-id-3"),
+                            createResultEntity(Software::new, "software-dedup-id-4",
+                                    "50|software-objectstore-id-4", "50|software-objectstore-id-4")
+                    ));
 
-            List<IdentifierMapping> result = ImportInformationSpaceJobUtils.mapToObjectStoreId(dedupRelation,
+            List<IdentifierMapping> result = ImportInformationSpaceJobUtils.produceObjectStoreId(dedupRelation,
                     sourceDataset,
                     sourceOtherResearchProduct,
                     sourcePublication,
                     sourceSoftware,
                     spark()).collect();
 
-            assertThat(result.size(), equalTo(identifierMappings.size()));
-            assertThat(result, hasItem(createIdentifierMapping("dataset-new-id-1", "dataset-objectstore-id-1")));
+            assertThat(result.size(), equalTo(14));
+            assertThat(result, hasItem(createIdentifierMapping("dataset-new-id-1", "50|dataset-objectstore-id-1")));
+            assertThat(result, hasItem(createIdentifierMapping("dataset-new-id-2", "dataset-original-id-2")));
+            assertThat(result, hasItem(createIdentifierMapping("dataset-new-id-3", "50|dataset-objectstore-id-3")));
             assertThat(result, hasItem(createIdentifierMapping("organization-new-id-1", "organization-original-id-1")));
-            assertThat(result, hasItem(createIdentifierMapping("orp-new-id-1", "orp-objectstore-id-1")));
+            assertThat(result, hasItem(createIdentifierMapping("orp-new-id-1", "50|orp-objectstore-id-1")));
+            assertThat(result, hasItem(createIdentifierMapping("orp-new-id-2", "orp-original-id-2")));
+            assertThat(result, hasItem(createIdentifierMapping("orp-new-id-3", "50|orp-objectstore-id-3")));
             assertThat(result, hasItem(createIdentifierMapping("project-new-id-1", "project-original-id-1")));
-            assertThat(result, hasItem(createIdentifierMapping("publication-new-id-1", "publication-objectstore-id-1")));
-            assertThat(result, hasItem(createIdentifierMapping("software-new-id-1", "software-objectstore-id-1")));
+            assertThat(result, hasItem(createIdentifierMapping("publication-new-id-1", "50|publication-objectstore-id-1")));
+            assertThat(result, hasItem(createIdentifierMapping("publication-new-id-2", "publication-original-id-2")));
+            assertThat(result, hasItem(createIdentifierMapping("publication-new-id-3", "50|publication-objectstore-id-3")));
+            assertThat(result, hasItem(createIdentifierMapping("software-new-id-1", "50|software-objectstore-id-1")));
+            assertThat(result, hasItem(createIdentifierMapping("software-new-id-2", "software-original-id-2")));
+            assertThat(result, hasItem(createIdentifierMapping("software-new-id-3", "50|software-objectstore-id-3")));
         }
     }
 
