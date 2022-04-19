8ebde9b9188f079c7fad0a0e814ec47df55a119a
Marek Horst <mhorst@icm.edu.pl>
2022-04-19 14:59:13 +0200

    Closes #1336: Rename citatations to references in ExtraInfo element
    Renaming ExtraInfo payload typology and name.

--- iis-common/src/main/java/eu/dnetlib/iis/common/model/extrainfo/ExtraInfoConstants.java
+++ iis-common/src/main/java/eu/dnetlib/iis/common/model/extrainfo/ExtraInfoConstants.java
@@ -11,9 +11,9 @@ public final class ExtraInfoConstants {
 	public static final String NAME_RESULT_STATISTICS = "result statistics";
 	public static final String NAME_AUTHOR_STATISTICS = "author statistics";
 	
-	public static final String NAME_REFERENCES = "result references";
+	public static final String NAME_REFERENCES = "result reference list";
 	
-	public static final String TYPOLOGY_REFERENCES = "references";
+	public static final String TYPOLOGY_REFERENCES = "reference list";
 	public static final String TYPOLOGY_STATISTICS = "statistics";
 	
 	public static final String CITATION_TYPE_OPENAIRE = "openaire";
