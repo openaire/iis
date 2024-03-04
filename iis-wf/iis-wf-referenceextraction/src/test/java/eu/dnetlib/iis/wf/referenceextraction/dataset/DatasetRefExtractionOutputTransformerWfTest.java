d919bae087d692d0446dfce44f672595ed5d8a63
Marek Horst <marek.horst@gmail.com>
2024-03-04 13:34:55 +0100

    Closes #1439: Remove project concept matching phase
    This commit removes support for matching concepts (listed in `import_project_concepts_context_ids_csv` IIS input parameter) with publications linked to projects associated with those concepts.
    
    The reason for the removal is this code is now overlapping with the community bulk tagging functionality implemented in a different module being a part of the provisioning chain.
    
    The result of the removal is manifested by the lack of the Oaf publication records encapsulating the concepts (defined as contexts) associated with publications in the output actionset identified with the `action_set_id_document_referencedProjects` input parameter. Relevant report metrics (`processing.referenceExtraction.concept.*`) are also removed.
    
    It is enough to list communities and research initiatives only among the concepts listed as `import_project_concepts_context_ids_csv` input parameter from now on. The listed concepts will be streamed to community TDM (text data mining) and research initiatives TDM algorithms only. No matching against publications linked to projects will be performed anymore.

--- iis-wf/iis-wf-referenceextraction/src/test/java/eu/dnetlib/iis/wf/referenceextraction/dataset/DatasetRefExtractionOutputTransformerWfTest.java
+++ iis-wf/iis-wf-referenceextraction/src/test/java/eu/dnetlib/iis/wf/referenceextraction/dataset/DatasetRefExtractionOutputTransformerWfTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 public class DatasetRefExtractionOutputTransformerWfTest extends AbstractOozieWorkflowTestCase {
 
     @Test
-	public void testProjectToConceptWorkflow() throws Exception {
+	public void testDatasetReferenceExtractionOutputTransformerWorkflow() throws Exception {
     	OozieWorkflowTestConfiguration wf = new OozieWorkflowTestConfiguration();
         wf.setTimeoutInSeconds(720);
         testWorkflow("eu/dnetlib/iis/wf/referenceextraction/dataset/output_transformer/test", wf);
