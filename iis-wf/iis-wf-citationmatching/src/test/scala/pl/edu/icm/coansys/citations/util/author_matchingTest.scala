d758cb5028345d6ece530386aa09361394c9a1cc
Marek Horst <marek.horst@gmail.com>
2024-10-15 17:17:27 +0200

    Closes #1475: Rewrite citation matching algorithm in spark 2.4
    Copying modified scala tests from citation-matching-coansys-code and ceon-scala-commons-lite in order to keep the transplanted code covered with tests.
    Tests were ported from junit4 to junit5.

---
+++ iis-wf/iis-wf-citationmatching/src/test/scala/pl/edu/icm/coansys/citations/util/author_matchingTest.scala
@@ -0,0 +1,40 @@
+/*
+ * This file is part of CoAnSys project.
+ * Copyright (c) 2012-2015 ICM-UW
+ * 
+ * CoAnSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * CoAnSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CoAnSys. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.edu.icm.coansys.citations.util
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import author_matching._
+
+/**
+ * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
+ */
+class author_matchingTest {
+  @Test
+  def matchingTokensTest() {
+    assertEquals(exactMatchingTokens("ala ma kota".split(" ").toList, "kot ma ale".split(" ").toList), List((1, 1)))
+    assertEquals(exactMatchingTokens("ala ma kota".split(" ").toList, "kota ala ma".split(" ").toList), List((0, 1), (1, 2), (2, 0)))
+  }
+
+  @Test
+  def roughMatchingTest() {
+    assertEquals(nonOverlappingMatching(Set((0, 0), (1, 1))), List((0, 0), (1, 1)))
+    assertEquals(nonOverlappingMatching(Set((0, 0), (1, 4), (2, 2), (3, 3), (5, 5))), List((0, 0), (2, 2), (3, 3), (5, 5)))
+  }
+}
---
+++ iis-wf/iis-wf-citationmatching/src/test/scala/pl/edu/icm/coansys/citations/util/normalizedAuthorTokensFromAuthorListTest.scala
@@ -0,0 +1,65 @@
+/*
+ * This file is part of CoAnSys project.
+ * Copyright (c) 2012-2015 ICM-UW
+ * 
+ * CoAnSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * CoAnSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CoAnSys. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.edu.icm.coansys.citations.util
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import pl.edu.icm.coansys.citations.util.misc.normalizedAuthorTokensFromAuthorList
+import pl.edu.icm.coansys.models.DocumentProtos.{BasicMetadata, Author}
+
+/**
+ * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
+ */
+class normalizedAuthorTokensFromAuthorListTest {
+  @Test
+  def noAuthorsTest() {
+    val meta = BasicMetadata.newBuilder().build()
+    assertEquals(normalizedAuthorTokensFromAuthorList(meta), Set())
+  }
+
+  @Test
+  def emptyAuthorTest() {
+    val author = Author.newBuilder().setKey("1").build()
+    val meta = BasicMetadata.newBuilder().addAuthor(author).build()
+    assertEquals(author.getName, "")
+    assertEquals(normalizedAuthorTokensFromAuthorList(meta), Set())
+  }
+
+  @Test
+  def authorWithSomeDataTest() {
+    val author = Author.newBuilder().setKey("1").setName("Kowalski").build()
+    val meta = BasicMetadata.newBuilder().addAuthor(author).build()
+    assertEquals(normalizedAuthorTokensFromAuthorList(meta), Set("kowalski"))
+  }
+
+  @Test
+  def specialCharsInNameTest() {
+    val author = Author.newBuilder().setKey("1").setName("Kowalski, Jan").build()
+    val meta = BasicMetadata.newBuilder().addAuthor(author).build()
+    assertEquals(normalizedAuthorTokensFromAuthorList(meta), Set("kowalski", "jan"))
+  }
+
+  @Test
+  def abbreviationsTest() {
+    val author1 = Author.newBuilder().setKey("1").setName("Qi Lu").build()
+    val author2 = Author.newBuilder().setKey("1").setName("J. Kowalski").build()
+    val meta = BasicMetadata.newBuilder().addAuthor(author1).addAuthor(author2).build()
+    assertEquals(normalizedAuthorTokensFromAuthorList(meta), Set("kowalski", "qi", "lu"))
+  }
+}
