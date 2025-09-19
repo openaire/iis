b492c20857e0c14443698e16d56fb0954df755d4
Marek Horst <marek.horst@gmail.com>
2025-09-19 14:00:32 +0200

    Closes #1565: Make sure all Oaf model classes are registered in Kryo when running InfoSpace importer
    Registering all the missing classes from the Oaf model.

--- iis-wf/iis-wf-import/src/main/java/eu/dnetlib/iis/wf/importer/infospace/OafModelUtils.java
+++ iis-wf/iis-wf-import/src/main/java/eu/dnetlib/iis/wf/importer/infospace/OafModelUtils.java
@@ -1,31 +1,44 @@
 package eu.dnetlib.iis.wf.importer.infospace;
 
+import eu.dnetlib.dhp.schema.oaf.AccessRight;
 import eu.dnetlib.dhp.schema.oaf.Author;
 import eu.dnetlib.dhp.schema.oaf.Context;
 import eu.dnetlib.dhp.schema.oaf.Country;
 import eu.dnetlib.dhp.schema.oaf.DataInfo;
 import eu.dnetlib.dhp.schema.oaf.Dataset;
 import eu.dnetlib.dhp.schema.oaf.Datasource;
+import eu.dnetlib.dhp.schema.oaf.EoscIfGuidelines;
 import eu.dnetlib.dhp.schema.oaf.ExternalReference;
 import eu.dnetlib.dhp.schema.oaf.ExtraInfo;
 import eu.dnetlib.dhp.schema.oaf.Field;
 import eu.dnetlib.dhp.schema.oaf.GeoLocation;
+import eu.dnetlib.dhp.schema.oaf.H2020Classification;
+import eu.dnetlib.dhp.schema.oaf.H2020Programme;
 import eu.dnetlib.dhp.schema.oaf.Instance;
+import eu.dnetlib.dhp.schema.oaf.InstanceTypeMapping;
 import eu.dnetlib.dhp.schema.oaf.Journal;
 import eu.dnetlib.dhp.schema.oaf.KeyValue;
+import eu.dnetlib.dhp.schema.oaf.Measure;
 import eu.dnetlib.dhp.schema.oaf.OAIProvenance;
 import eu.dnetlib.dhp.schema.oaf.Oaf;
 import eu.dnetlib.dhp.schema.oaf.OafEntity;
+import eu.dnetlib.dhp.schema.oaf.OpenAccessColor;
+import eu.dnetlib.dhp.schema.oaf.OpenAccessRoute;
 import eu.dnetlib.dhp.schema.oaf.Organization;
 import eu.dnetlib.dhp.schema.oaf.OriginDescription;
 import eu.dnetlib.dhp.schema.oaf.OtherResearchProduct;
+import eu.dnetlib.dhp.schema.oaf.Person;
+import eu.dnetlib.dhp.schema.oaf.PersonTopic;
+import eu.dnetlib.dhp.schema.oaf.Pid;
 import eu.dnetlib.dhp.schema.oaf.Project;
 import eu.dnetlib.dhp.schema.oaf.Publication;
 import eu.dnetlib.dhp.schema.oaf.Qualifier;
+import eu.dnetlib.dhp.schema.oaf.RawAuthorAffiliation;
 import eu.dnetlib.dhp.schema.oaf.Relation;
 import eu.dnetlib.dhp.schema.oaf.Result;
 import eu.dnetlib.dhp.schema.oaf.Software;
 import eu.dnetlib.dhp.schema.oaf.StructuredProperty;
+import eu.dnetlib.dhp.schema.oaf.Subject;
 
 /**
  * @author mhorst
@@ -38,32 +51,45 @@ public class OafModelUtils {
      */
     public static final Class<?>[] provideOafClasses() {
         return new Class[]{
+                AccessRight.class,
                 Author.class,
                 Context.class,
                 Country.class,
                 DataInfo.class,
                 Dataset.class,
                 Datasource.class,
+                EoscIfGuidelines.class,
                 ExternalReference.class,
                 ExtraInfo.class,
                 Field.class,
                 GeoLocation.class,
+                H2020Classification.class,
+                H2020Programme.class,
                 Instance.class,
+                InstanceTypeMapping.class,
                 Journal.class,
                 KeyValue.class,
+                Measure.class,
                 Oaf.class,
                 OafEntity.class,
                 OAIProvenance.class,
+                OpenAccessColor.class,
+                OpenAccessRoute.class,
                 Organization.class,
                 OriginDescription.class,
                 OtherResearchProduct.class,
+                Person.class,
+                PersonTopic.class,
+                Pid.class,
                 Project.class,
                 Publication.class,
                 Qualifier.class,
+                RawAuthorAffiliation.class,
                 Relation.class,
                 Result.class,
                 Software.class,
-                StructuredProperty.class
+                StructuredProperty.class,
+                Subject.class
         };
     }
     
