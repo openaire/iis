61bd981d659aeb0659719ecee2da8c260079c4e3
johnfouf <johnfouf@gmail.com>
2026-02-25 15:51:04 +0200

    1st try to increase recall - utilize complete patent mentions better

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
@@ -12,12 +12,15 @@ select jdict('documentId', docid, 'appln_nr', id, 'patentcode',patentcode, 'conf
 select  docid, patents.c1 as id, patents.c2 as patentcode, prev||" "||middle||" "||next as context from 
 (setschema 'docid,prev,middle,next' select c1 as docid, textwindow2s(keywords(c2),7,1,3, "(?:\D|\b)[A-Z]?\d{6,}(\b|\D)") from (setschema 'c1,c2' select * from pubs)), patents
 where regexpr("(\d{6,})", middle) = normal 
-and (     regexprmatches("patent",lower(context)) and      (     (       
+and ( ( (     regexprmatches("patent",lower(context)) and      (     (       
 regexprmatches("\b"||c4||"\b",prev||" "||next) and not (regexprmatches((select jmergeregexp(jgroup("\b"||jurisdiction||"(?:\b|\d+)")) from jurisdictions where jurisdiction != c4),string_split_value(j2s(prev,middle), c4)))     )
    or regexprmatches("\b"||c4,middle)     )  and regexprmatches(c2, middle)     )
    
    and (not regexprmatches("holotype|journal pone|journal pntd|paratype|scientometrics|specimen|dissection|\bnih\b|hepth|barcode|\bstrain|accession|\bbacter\b|patent ductus|patent foramen|arxiv|cern|biol|clin|letters|report",lower(context))) 
    and (not regexprmatches("[0-9]\."||normal,middle) )
+   )
+   or regexprmatches(lower(middle),lower(c4||c2||c5))
+   )
 );
 
 
