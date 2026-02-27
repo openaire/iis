defb7d363fe55245fcaae798dcaf5780e7c5118e
Marek Horst <marek.horst@gmail.com>
2026-02-27 17:27:17 +0100

    Closes #1588: Make the IIS compliant with spark4 and kubernetes environment
    Fixing `iis-3rdparty-coansys-citation-matching` compilation issues. Quite a long list of issues to be fixed on different fronts:
    * upgrading `scala-maven-plugin` to become compatible with java17
    * upgrading scala version to `2.13.17`
    * scala classes compilation (after required upgrade of scala version), sources had to be modified to align with the new scala/spark version
    * Mockito classes location has changed in the newer version so the reference to `org.mockito.Matchers` has to be replaced with `org.mockito.ArgumentMatchers`

--- iis-3rdparty-coansys-citation-matching/src/main/java/pl/edu/icm/coansys/citations/ScalaIterableConverter.java
+++ iis-3rdparty-coansys-citation-matching/src/main/java/pl/edu/icm/coansys/citations/ScalaIterableConverter.java
@@ -2,7 +2,7 @@ package pl.edu.icm.coansys.citations;
 
 import java.io.Serializable;
 
-import scala.collection.JavaConversions;
+import scala.jdk.javaapi.CollectionConverters;
 
 /**
  * Converter of {@link scala.collection.Iterable} to {@link java.lang.Iterable}
@@ -16,11 +16,11 @@ class ScalaIterableConverter implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
-     * Converts {@link scala.collection.Iterable} to {@link java.lang.Iterable}. The method is not static (as {@link JavaConversions#asJavaIterable(scala.collection.Iterable)}) 
+     * Converts {@link scala.collection.Iterable} to {@link java.lang.Iterable}. The method is not static (as {@link CollectionConverters#asJava(scala.collection.Iterable)})
      * so it can be used as a service method and it is easier to test.
      */
     <T> Iterable<T> convertToJavaIterable(scala.collection.Iterable<T> scalaIterable) {
-        return JavaConversions.asJavaIterable(scalaIterable);
+        return CollectionConverters.asJava(scalaIterable);
     }
     
 }
