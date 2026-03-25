a2e9d727ad8b228d2fc308cc6733a6dea051a77f
Marek Horst <marek.horst@gmail.com>
2026-03-25 12:03:39 +0100

    Closes #1554: Implement a workflow responsible for importing HTML landing pages obtained by the PDF Aggregation System and sending them to the mining algorithm
    Introducing HTML payloads importer producing CSV file at output. It relies on PDF Aggregation Service hive tables similarly to `HiveBasedDocumentContentUrlImporterJob`.
    The output of this workflow is intended to be used by the `affextraction` subworkflow responsible for importing HTML contents from S3 and performing HTML parsing which so far relied on a statically provided CSV file which now is being generated on the fly.

---
+++ iis-wf/iis-wf-import/src/main/java/eu/dnetlib/iis/wf/importer/content/HiveBasedHtmlPayloadsMetadataImporterJob.java
@@ -0,0 +1,116 @@
+package eu.dnetlib.iis.wf.importer.content;
+
+import static eu.dnetlib.iis.common.spark.SparkSessionSupport.runWithHiveEnabledSparkSession;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import com.google.common.collect.Lists;
+
+import eu.dnetlib.iis.common.java.io.HdfsUtils;
+import eu.dnetlib.iis.common.report.ReportEntryFactory;
+import eu.dnetlib.iis.common.schemas.ReportEntry;
+import eu.dnetlib.iis.common.spark.SparkConfHelper;
+import pl.edu.icm.sparkutils.avro.SparkAvroSaver;
+
+
+/**
+ * Html payloads metadata importer reading data from hive table being a part of pdf aggregation subsystem. 
+ * 
+ * @author mhorst
+ *
+ */
+public class HiveBasedHtmlPayloadsMetadataImporterJob {
+
+    private static SparkAvroSaver avroSaver = new SparkAvroSaver();
+    
+    private static final String COUNTER_IMPORTED_RECORDS_TOTAL = "import.crawled.html.urls.fromAggregator";
+
+    private static final String DATABASE_NAME_PLACEHOLDER = "${databaseName}";
+    
+    public static void main(String[] args) throws Exception {
+
+        HiveBasedHtmlPayloadsImporterJobParameters params = new HiveBasedHtmlPayloadsImporterJobParameters();
+        JCommander jcommander = new JCommander(params);
+        jcommander.parse(args);
+
+        SparkConf conf = SparkConfHelper.withKryo(new SparkConf());
+        if (!StringUtils.isEmpty(params.hiveMetastoreUris)) {
+            conf.set("hive.metastore.uris", params.hiveMetastoreUris);    
+        }
+        
+        runWithHiveEnabledSparkSession(conf, params.isSparkSessionShared, sparkSession -> {
+            
+            HdfsUtils.remove(sparkSession.sparkContext().hadoopConfiguration(), params.outputPath);
+            HdfsUtils.remove(sparkSession.sparkContext().hadoopConfiguration(), params.outputReportPath);
+            
+            Dataset<Row> result = sparkSession.sql(generateQuery(params.inputDatabaseName));
+            
+            
+            JavaRDD<ReportEntry> reports = generateReportEntries(sparkSession, result.count()); 
+
+            avroSaver.saveJavaRDD(reports, ReportEntry.SCHEMA$, params.outputReportPath);
+
+            result.write().option("header", "true").csv(params.outputPath);
+        });
+    }
+
+    // @formatter:off
+    private static final String QUERY_TEMPLATE = String.join("\n",
+            "select pid, pid_type, dedupid, id, original_url, actual_url,",
+            "  archive_s3_location, html_filename, html_hash, html_size",
+            "from (",
+            "  select pub.pid, pub.pid_type, pub.dedupid, p.id, p.original_url, p.actual_url,",
+            "    SUBSTR(p.`location`, 1, INSTR(p.`location`, '.tar.gz') + LENGTH('.tar.gz') - 1) AS archive_s3_location,",
+            "    SUBSTR(p.`location`, INSTR(p.`location`, '.tar.gz/') + LENGTH('.tar.gz/')) AS html_filename,",
+            "    p.`hash` as html_hash, p.`size` as html_size",
+            "    , row_number() over (partition by p.id, p.original_url, p.actual_url, p.`location` order by",
+            "      (case when pub.pid_type = 'doi' then 2 when pub.pid_type is not null then 1 else 0 end) desc",
+            "    ) as rn",
+            "  from ${databaseName}.html_payload p",
+            "  left outer join ${databaseName}.publication pub",
+            "    on pub.id = p.id and pub.url = p.original_url",
+            ") subq",
+            "where subq.rn = 1"
+    );
+    // @formatter:on
+
+    private static String generateQuery(String databaseName) {
+        return QUERY_TEMPLATE.replace(DATABASE_NAME_PLACEHOLDER, databaseName);
+    }
+
+    private static JavaRDD<ReportEntry> generateReportEntries(SparkSession sparkSession, long recordsCount) {
+        return sparkSession.createDataset(
+                Lists.newArrayList(
+                        ReportEntryFactory.createCounterReportEntry(COUNTER_IMPORTED_RECORDS_TOTAL, recordsCount)),
+                Encoders.kryo(ReportEntry.class)).javaRDD();
+    }
+        
+    @Parameters(separators = "=")
+    private static class HiveBasedHtmlPayloadsImporterJobParameters {
+        
+        @Parameter(names = "-sharedSparkSession")
+        private Boolean isSparkSessionShared = Boolean.FALSE;
+        
+        @Parameter(names = "-inputDatabaseName", required = true)
+        private String inputDatabaseName;
+        
+        @Parameter(names = "-hiveMetastoreUris", required = true)
+        private String hiveMetastoreUris;
+        
+        @Parameter(names = "-outputPath", required = true)
+        private String outputPath;
+
+        @Parameter(names = "-outputReportPath", required = true)
+        private String outputReportPath;
+    }
+    
+}
