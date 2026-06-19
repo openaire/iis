71c115bf99ffe66fd195ddc93d4372dc3fccbedb
johnfouf <johnfouf@gmail.com>
2026-06-20 00:02:42 +0300

    Update SQL query to select lens_id instead of appln_nr

--- iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
+++ iis-wf/iis-wf-referenceextraction/src/main/resources/eu/dnetlib/iis/wf/referenceextraction/patent/main_sqlite/oozie_app/lib/scripts/patents.sql
@@ -8,7 +8,7 @@ stdinput() );
 
 
 --create temp table results as select * from (
-select jdict('documentId', docid, 'appln_nr', id, 'patentcode',patentcode, 'confidenceLevel', 0.8,'textsnippet',context) from (
+select jdict('documentId', docid, 'lens_id', id, 'confidenceLevel', 0.8,'textsnippet',context) from (
 select  docid, patents.c1 as id, patents.c2 as patentcode, prev||" "||middle||" "||next as context from 
 (setschema 'docid,prev,middle,next' select c1 as docid, textwindow2s(keywords(c2),7,1,3, "(?:\D|\b)[A-Z]?\d{6,}(\b|\D)") from (setschema 'c1,c2' select * from pubs)), patents
 where regexpr("(\d{6,})", middle) = normal 
